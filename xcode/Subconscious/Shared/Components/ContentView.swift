860f3aef5dde2e1c65f008c2f1dbcc659b9bf509
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-05-21 10:21:47 -0700

    Move app model up, treat ContentView as equatable
    This prevents ContentView from re-rendering if AppModel is the same.
    
    Right now, this doesn't change much, but in future we can bring some
    service-related state up to the app level, and move ContentView-related
    state into a content field of the AppModel. This will allow us to use
    Elm-style update functions to manage the lifecycle of things like search
    indexes.
    
    It does mean a static preview for ContentView, but that's ok. We're
    getting to the point where you want to do a full build to test anyway.

--- xcode/Subconscious/Shared/Components/ContentView.swift
+++ xcode/Subconscious/Shared/Components/ContentView.swift
@@ -10,202 +10,8 @@ import Foundation
 import Combine
 import os
 
-
-//  MARK: AppStore typealias
-typealias AppStore = Store<AppModel, AppAction, AppEnvironment>
-
-
-//  MARK: App Actions
-/// Actions that may be taken on the Store
-enum AppAction {
-    case editor(_ action: EditorAction)
-    case searchBar(_ action: SubSearchBarAction)
-    case search(_ action: SearchAction)
-    case suggestionTokens(_ action: TextTokenBarAction)
-    case appear
-    case edit(_ document: SubconsciousDocument)
-    case commitQuery(_ query: String)
-    case setLiveQuery(_ query: String)
-    case setEditorPresented(_ isPresented: Bool)
-    case setSuggestions(_ suggestions: [Suggestion])
-    case saveThread(SubconsciousDocument)
-    case warning(_ message: String)
-    case info(_ message: String)
-}
-
-//  MARK: Tagging functions
-
-func tagEditorAction(_ action: EditorAction) -> AppAction {
-    switch action {
-    case .requestEditorUnpresent:
-        return .setEditorPresented(false)
-    case .requestSave(let thread):
-        return .saveThread(thread)
-    default:
-        return .editor(action)
-    }
-}
-
-func tagSearchBarAction(_ action: SubSearchBarAction) -> AppAction {
-    switch action {
-    case .commitQuery(let query):
-        return .commitQuery(query)
-    case .setLiveQuery(let query):
-        return .setLiveQuery(query)
-    default:
-        return .searchBar(action)
-    }
-}
-
-func tagSearchAction(_ action: SearchAction) -> AppAction {
-    switch action {
-    case .requestEdit(let document):
-        return .edit(document)
-    default:
-        return .search(action)
-    }
-}
-
-func tagSuggestionListAction(_ action: SuggestionListAction) -> AppAction {
-    switch action {
-    case .select(let suggestion):
-        return .commitQuery(suggestion.description)
-    }
-}
-
-func tagSuggestionTokensAction(_ action: TextTokenBarAction) -> AppAction {
-    switch action {
-    case .select(let text):
-        return .commitQuery(text)
-    default:
-        return .suggestionTokens(action)
-    }
-}
-
-//  MARK: App State
-/// Central source of truth for all shared app state
-struct AppModel: Equatable {
-    var searchBar = SubSearchBarModel()
-    var search = SearchModel(documents: [])
-    /// Semi-permanent suggestions that show up as tokens in the search view.
-    /// We don't differentiate between types of token, so these are all just strings.
-    var suggestionTokens = TextTokenBarModel()
-    /// Live-as-you-type suggestions
-    var suggestions: [Suggestion] = []
-    var editor = EditorModel()
-    var isEditorPresented = false
-}
-
-//  MARK: App Reducer
-/// Reducer for state
-/// Mutates state in response to actions, returning effects
-func updateApp(
-    state: inout AppModel,
-    action: AppAction,
-    environment: AppEnvironment
-) -> AnyPublisher<AppAction, Never> {
-    switch action {
-    case .editor(let action):
-        return updateEditor(
-            state: &state.editor,
-            action: action,
-            environment: environment
-        ).map(tagEditorAction).eraseToAnyPublisher()
-    case .searchBar(let action):
-        return updateSubSearchBar(
-            state: &state.searchBar,
-            action: action
-        ).map(tagSearchBarAction).eraseToAnyPublisher()
-    case .search(let action):
-        return updateSearch(
-            state: &state.search,
-            action: action,
-            environment: environment
-        ).map(tagSearchAction).eraseToAnyPublisher()
-    case .suggestionTokens(let action):
-        return updateTextTokenBar(
-            state: &state.suggestionTokens,
-            action: action,
-            environment: BasicEnvironment(logger: environment.logger)
-        ).map(tagSuggestionTokensAction).eraseToAnyPublisher()
-    case .appear:
-        let dir = environment.documentService.documentDirectory?
-            .absoluteString ?? ""
-        environment.logger.info(
-            """
-            AppAction.appear
-            User Directory: \(dir)
-            """
-        )
-        let initialQueryEffect = Just(AppAction.commitQuery(""))
-        let suggestionTokensEffect = environment
-            .fetchSuggestionTokens()
-            .map({ suggestions in
-                AppAction.suggestionTokens(.setTokens(suggestions))
-            })
-        return Publishers.Merge(
-            initialQueryEffect,
-            suggestionTokensEffect
-        ).eraseToAnyPublisher()
-    case .edit(let document):
-        state.isEditorPresented = true
-        return Just(.editor(.edit(document))).eraseToAnyPublisher()
-    case .setEditorPresented(let isPresented):
-        state.isEditorPresented = isPresented
-    case .commitQuery(let query):
-        let searchBarEffect = updateSubSearchBar(
-            state: &state.searchBar,
-            action: .commitQuery(query)
-        ).map(tagSearchBarAction)
-
-        let suggestionsEffect = environment
-            .fetchSuggestions(query: query)
-            .map(AppAction.setSuggestions)
-
-        let documentQueryEffect = environment
-            .documentService.query(query: query)
-            .map({ documents in AppAction.search(.setItems(documents)) })
-        
-        return Publishers.Merge3(
-            searchBarEffect,
-            suggestionsEffect,
-            documentQueryEffect
-        ).eraseToAnyPublisher()
-    case .setLiveQuery(let query):
-        let searchBarEffect = updateSubSearchBar(
-            state: &state.searchBar,
-            action: .setLiveQuery(query)
-        ).map(tagSearchBarAction)
-
-        let suggestionsEffect = environment
-            .fetchSuggestions(query: query)
-            .map(AppAction.setSuggestions)
-
-        return Publishers.Merge(
-            searchBarEffect,
-            suggestionsEffect
-        ).eraseToAnyPublisher()
-    case .setSuggestions(let suggestions):
-        state.suggestions = suggestions
-    case .saveThread(let thread):
-        return environment.documentService.write(thread)
-            .map({ AppAction.info("Saved thread") })
-            .eraseToAnyPublisher()
-    case .warning(let message):
-        environment.logger.warning("\(message)")
-    case .info(let message):
-        environment.logger.info("\(message)")
-    }
-    return Empty().eraseToAnyPublisher()
-}
-
-//  MARK: ContentView
 struct ContentView: View, Equatable {
-    static func == (lhs: ContentView, rhs: ContentView) -> Bool {
-        lhs.store.state == rhs.store.state
-    }
-    
-    @ObservedObject var store: AppStore
+    var store: ViewStore<AppModel, AppAction>
 
     var body: some View {
         VStack(spacing: 0) {
@@ -308,10 +114,17 @@ struct ContentView: View, Equatable {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(store: AppStore(
+        let store = AppStore(
             state: .init(),
             reducer: updateApp,
             environment: .init()
-        ))
+        )
+
+        return ContentView(
+            store: ViewStore(
+                state: store.state,
+                send: store.send
+            )
+        )
     }
 }
---
+++ xcode/Subconscious/Shared/Components/SubconsciousApp.swift
@@ -0,0 +1,224 @@
+//
+//  SubconsciousApp.swift
+//  Shared
+//
+//  Created by Gordon Brander on 4/4/21.
+//
+
+import SwiftUI
+import Combine
+
+//  MARK: AppStore typealias
+typealias AppStore = Store<AppModel, AppAction, AppEnvironment>
+
+
+//  MARK: App Actions
+/// Actions that may be taken on the Store
+enum AppAction {
+    case editor(_ action: EditorAction)
+    case searchBar(_ action: SubSearchBarAction)
+    case search(_ action: SearchAction)
+    case suggestionTokens(_ action: TextTokenBarAction)
+    case appear
+    case edit(_ document: SubconsciousDocument)
+    case commitQuery(_ query: String)
+    case setLiveQuery(_ query: String)
+    case setEditorPresented(_ isPresented: Bool)
+    case setSuggestions(_ suggestions: [Suggestion])
+    case saveThread(SubconsciousDocument)
+    case syncSearchIndex
+    case warning(_ message: String)
+    case info(_ message: String)
+}
+
+//  MARK: Tagging functions
+
+func tagEditorAction(_ action: EditorAction) -> AppAction {
+    switch action {
+    case .requestEditorUnpresent:
+        return .setEditorPresented(false)
+    case .requestSave(let thread):
+        return .saveThread(thread)
+    default:
+        return .editor(action)
+    }
+}
+
+func tagSearchBarAction(_ action: SubSearchBarAction) -> AppAction {
+    switch action {
+    case .commitQuery(let query):
+        return .commitQuery(query)
+    case .setLiveQuery(let query):
+        return .setLiveQuery(query)
+    default:
+        return .searchBar(action)
+    }
+}
+
+func tagSearchAction(_ action: SearchAction) -> AppAction {
+    switch action {
+    case .requestEdit(let document):
+        return .edit(document)
+    default:
+        return .search(action)
+    }
+}
+
+func tagSuggestionListAction(_ action: SuggestionListAction) -> AppAction {
+    switch action {
+    case .select(let suggestion):
+        return .commitQuery(suggestion.description)
+    }
+}
+
+func tagSuggestionTokensAction(_ action: TextTokenBarAction) -> AppAction {
+    switch action {
+    case .select(let text):
+        return .commitQuery(text)
+    default:
+        return .suggestionTokens(action)
+    }
+}
+
+//  MARK: App State
+/// Central source of truth for all shared app state
+struct AppModel: Equatable {
+    var searchBar = SubSearchBarModel()
+    var search = SearchModel(documents: [])
+    /// Semi-permanent suggestions that show up as tokens in the search view.
+    /// We don't differentiate between types of token, so these are all just strings.
+    var suggestionTokens = TextTokenBarModel()
+    /// Live-as-you-type suggestions
+    var suggestions: [Suggestion] = []
+    var editor = EditorModel()
+    var isEditorPresented = false
+}
+
+//  MARK: App Reducer
+/// Reducer for state
+/// Mutates state in response to actions, returning effects
+func updateApp(
+    state: inout AppModel,
+    action: AppAction,
+    environment: AppEnvironment
+) -> AnyPublisher<AppAction, Never> {
+    switch action {
+    case .editor(let action):
+        return updateEditor(
+            state: &state.editor,
+            action: action,
+            environment: environment
+        ).map(tagEditorAction).eraseToAnyPublisher()
+    case .searchBar(let action):
+        return updateSubSearchBar(
+            state: &state.searchBar,
+            action: action
+        ).map(tagSearchBarAction).eraseToAnyPublisher()
+    case .search(let action):
+        return updateSearch(
+            state: &state.search,
+            action: action,
+            environment: environment
+        ).map(tagSearchAction).eraseToAnyPublisher()
+    case .suggestionTokens(let action):
+        return updateTextTokenBar(
+            state: &state.suggestionTokens,
+            action: action,
+            environment: BasicEnvironment(logger: environment.logger)
+        ).map(tagSuggestionTokensAction).eraseToAnyPublisher()
+    case .appear:
+        let dir = environment.documentService.fileManager.documentDirectoryUrl?
+            .absoluteString ?? ""
+        environment.logger.info(
+            """
+            AppAction.appear
+            User Directory: \(dir)
+            """
+        )
+        let initialQueryEffect = Just(AppAction.commitQuery(""))
+        let suggestionTokensEffect = environment
+            .fetchSuggestionTokens()
+            .map({ suggestions in
+                AppAction.suggestionTokens(.setTokens(suggestions))
+            })
+        let syncSearchIndexEffect = Just(AppAction.syncSearchIndex)
+        return Publishers.Merge3(
+            initialQueryEffect,
+            suggestionTokensEffect,
+            syncSearchIndexEffect
+        ).eraseToAnyPublisher()
+    case .edit(let document):
+        state.isEditorPresented = true
+        return Just(.editor(.edit(document))).eraseToAnyPublisher()
+    case .setEditorPresented(let isPresented):
+        state.isEditorPresented = isPresented
+    case .commitQuery(let query):
+        let searchBarEffect = updateSubSearchBar(
+            state: &state.searchBar,
+            action: .commitQuery(query)
+        ).map(tagSearchBarAction)
+
+        let suggestionsEffect = environment
+            .fetchSuggestions(query: query)
+            .map(AppAction.setSuggestions)
+
+        let documentQueryEffect = environment
+            .documentService.query(query: query)
+            .map({ documents in AppAction.search(.setItems(documents)) })
+        
+        return Publishers.Merge3(
+            searchBarEffect,
+            suggestionsEffect,
+            documentQueryEffect
+        ).eraseToAnyPublisher()
+    case .setLiveQuery(let query):
+        let searchBarEffect = updateSubSearchBar(
+            state: &state.searchBar,
+            action: .setLiveQuery(query)
+        ).map(tagSearchBarAction)
+
+        let suggestionsEffect = environment
+            .fetchSuggestions(query: query)
+            .map(AppAction.setSuggestions)
+
+        return Publishers.Merge(
+            searchBarEffect,
+            suggestionsEffect
+        ).eraseToAnyPublisher()
+    case .setSuggestions(let suggestions):
+        state.suggestions = suggestions
+    case .saveThread(let thread):
+        return environment.documentService.write(thread)
+            .map({ AppAction.info("Saved thread") })
+            .eraseToAnyPublisher()
+    case .syncSearchIndex:
+        _ = environment.searchService.syncIndex()
+    case .warning(let message):
+        environment.logger.warning("\(message)")
+    case .info(let message):
+        environment.logger.info("\(message)")
+    }
+    return Empty().eraseToAnyPublisher()
+}
+
+
+//  MARK: App main View
+@main
+struct SubconsciousApp: App {
+    @StateObject private var store: AppStore = AppStore(
+        state: .init(),
+        reducer: updateApp,
+        environment: .init()
+    )
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView(
+                store: ViewStore(
+                    state: store.state,
+                    send: store.send
+                )
+            ).equatable()
+        }
+    }
+}
