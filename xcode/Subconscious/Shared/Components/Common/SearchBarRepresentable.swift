bfc3b8314ad9bdce25d823dfdd454ab237b932a0
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-06-30 19:14:44 -0700

    Working through UISearchBar state transitions
    ...using coordinator

--- xcode/Subconscious/Shared/Components/Common/SearchBarRepresentable.swift
+++ xcode/Subconscious/Shared/Components/Common/SearchBarRepresentable.swift
@@ -25,7 +25,7 @@ struct SearchBarRepresentable: UIViewRepresentable {
         init(_ representable: SearchBarRepresentable) {
             self.representable = representable
         }
-
+        
         func searchBar(
             _ searchBar: UISearchBar,
             textDidChange searchText: String
@@ -33,47 +33,92 @@ struct SearchBarRepresentable: UIViewRepresentable {
             representable.text = searchText
             representable.onCommit(searchText)
         }
-
+        
+        func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+            searchBar.setShowsCancelButton(true, animated: true)
+            representable.isFocused = true
+            representable.onFocus()
+        }
+        
         func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+            searchBar.setShowsCancelButton(false, animated: true)
+            searchBar.text = representable.initial
+            representable.isFocused = false
             representable.onCancel()
         }
 
         func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+            searchBar.setShowsCancelButton(false, animated: true)
+            representable.isFocused = false
             representable.onSubmit(searchBar.text ?? "")
-            UIApplication.shared.endEditing()
         }
     }
 
-    @Binding var text: String
+    private var isFocused = false
+    var text: String
+    var initial: String
     var placeholder: String
-    var showsCancelButton: Bool
     var onCommit: (String) -> Void
     var onSubmit: (String) -> Void
+    var onFocus: () -> Void
     var onCancel: () -> Void
 
+    init(
+        text: String,
+        initial: String,
+        placeholder: String,
+        onFocus: @escaping () -> Void,
+        onCommit: @escaping (String) -> Void,
+        onSubmit: @escaping (String) -> Void,
+        onCancel: @escaping () -> Void
+    ) {
+        self.text = text
+        self.initial = initial
+        self.placeholder = placeholder
+        self.onFocus = onFocus
+        self.onCommit = onCommit
+        self.onSubmit = onSubmit
+        self.onCancel = onCancel
+    }
+    
     func makeUIView(context: Context) -> UISearchBar {
         let searchBar = UISearchBar()
         searchBar.placeholder = placeholder
-        searchBar.showsCancelButton = showsCancelButton
+        searchBar.showsCancelButton = false
         searchBar.delegate = context.coordinator
         return searchBar
     }
 
-    func updateUIView(_ uiView: UISearchBar, context: Context) {
-        uiView.text = text
+    func updateUIView(_ searchBar: UISearchBar, context: Context) {
+        searchBar.text = text
+        searchBar.placeholder = placeholder
+        if isFocused {
+            searchBar.becomeFirstResponder()
+        } else {
+            searchBar.resignFirstResponder()
+        }
     }
 
     func makeCoordinator() -> SearchBarRepresentable.Coordinator {
         Coordinator(self)
+    }
+
+    func focused(_ isFocused: Bool) -> some View {
+        var view = self
+        view.isFocused = isFocused
+        return view
     }
 }
 
 struct SearchBarRepresentablePreview: PreviewProvider {
     static var previews: some View {
         SearchBarRepresentable(
-            text: .constant("Text"),
+            text: "Text",
+            initial: "",
             placeholder: "Placeholder",
-            showsCancelButton: false,
+            onFocus: {},
             onCommit: { text in },
             onSubmit: { text in },
             onCancel: {}
