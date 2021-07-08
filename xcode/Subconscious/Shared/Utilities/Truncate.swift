c7d8a2da122cf86c463db248f519df6ba7c66fd1
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-08 13:30:26 -0700

    Add utilities for nice text truncation

---
+++ xcode/Subconscious/Shared/Utilities/Truncate.swift
@@ -0,0 +1,26 @@
+//
+//  Truncate.swift
+//  Subconscious (iOS)
+//
+//  Created by Gordon Brander on 7/8/21.
+//
+
+import Foundation
+
+struct Truncate {
+    /// Determine if a character is a pseudo-sentence break character.
+    static func isPseudoSentenceBreak(_ character: Character) -> Bool {
+        return character.isNewline || character == "." || character == ";"
+    }
+
+    /// Extracts the first "pseudo-sentence" from some text.
+    /// We use newlines, periods, and semicolons to denote a pseudosentence break.
+    static func getFirstPseudoSentence(_ text: String) -> String {
+        let i = text.firstIndex(where: isPseudoSentenceBreak)
+        if let i = i {
+            return String(text.prefix(upTo: i))
+        } else {
+            return text
+        }
+    }
+}
