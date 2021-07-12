d03c56230686ea047ac37152c8008b3c5d9bef02
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-12 13:14:57 -0700

    Clean up TextViewRepresentable
    Add some useful decoration methods as well

--- xcode/Subconscious/Shared/Components/Common/TextViewRepresentable.swift
+++ xcode/Subconscious/Shared/Components/Common/TextViewRepresentable.swift
@@ -17,59 +17,31 @@ struct TextViewRepresentable: UIViewRepresentable {
         
         func textViewDidChange(_ view: UITextView) {
             representable.text = view.text
-            representable.onChange()
-        }
-
-        func textViewDidBeginEditing(_ textView: UITextView) {
-            representable.onFocus()
-        }
-        
-        func textViewDidEndEditing(_ textView: UITextView) {
-            representable.onBlur()
         }
     }
-
+    
     private var isFocused = false
-    private var intrinsicSize: CGSize?
+    private var font: UIFont = UIFont.preferredFont(forTextStyle: .body)
+    private var textContainerInset: UIEdgeInsets = .zero
     @Binding var text: String
-    var onChange: () -> Void
-    var onFocus: () -> Void
-    var onBlur: () -> Void
 
     init(
-        text: Binding<String>,
-        onChange: @escaping () -> Void,
-        onFocus: @escaping () -> Void,
-        onBlur: @escaping () -> Void
+        text: Binding<String>
     ) {
         self._text = text
-        self.onChange = onChange
-        self.onFocus = onFocus
-        self.onBlur = onBlur
     }
     
     func makeUIView(context: Context) -> UITextView {
         let view = UITextView()
         view.text = text
-        view.font = UIFont.preferredFont(forTextStyle: .body)
-        view.isUserInteractionEnabled = true
-        view.isEditable = true
-        // Zero out inner padding
-        view.textContainerInset = .zero
+        view.font = font
+        // Set inner padding
+        view.textContainerInset = textContainerInset
         // Remove that last bit of inner padding.
         // Text in view should now be flush with view edge.
         // This puts you in full control of view padding.
         view.textContainer.lineFragmentPadding = 0
         view.backgroundColor = .clear
-        
-        view.backgroundColor = UIColor.lightGray
-        
-        view.isScrollEnabled = false
-        view.showsVerticalScrollIndicator = true
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.textContainer.maximumNumberOfLines = 0
-        view.textContainer.lineBreakMode = .byWordWrapping
-        view.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
         
         if isFocused {
             view.becomeFirstResponder()
@@ -80,6 +52,18 @@ struct TextViewRepresentable: UIViewRepresentable {
     }
 
     func updateUIView(_ view: UITextView, context: Context) {
+        if view.text != text {
+            view.text = text
+        }
+
+        if view.font != font {
+            view.font = font
+        }
+
+        if view.textContainerInset != textContainerInset {
+            view.textContainerInset = textContainerInset
+        }
+
         if isFocused {
             view.becomeFirstResponder()
         } else {
@@ -90,7 +74,24 @@ struct TextViewRepresentable: UIViewRepresentable {
     func makeCoordinator() -> TextViewRepresentable.Coordinator {
         Coordinator(self)
     }
+    
+    func font(_ font: UIFont) -> Self {
+        var view = self
+        view.font = font
+        return view
+    }
 
+    func insets(_ inset: EdgeInsets) -> Self {
+        var view = self
+        view.textContainerInset = UIEdgeInsets(
+            top: inset.top,
+            left: inset.leading,
+            bottom: inset.bottom,
+            right: inset.trailing
+        )
+        return view
+    }
+    
     /// Focus view
     func focused(_ isFocused: Bool) -> Self {
         var view = self
@@ -102,32 +103,9 @@ struct TextViewRepresentable: UIViewRepresentable {
 struct TextViewRepresentablePreview: PreviewProvider {
     static var previews: some View {
         VStack {
-            GeometryReader { geometry in
-                VStack {
-                TextViewRepresentable(
-                    text: .constant("Text"),
-                    onChange: { },
-                    onFocus: {  },
-                    onBlur: {  }
-                )
-                .focused(true)
-                .frame(width: geometry.size.width)
-                .fixedSize()
-                .border(Color.red, width: 1)
-
-                    TextViewRepresentable(
-                        text: .constant("Text"),
-                        onChange: { },
-                        onFocus: {  },
-                        onBlur: {  }
-                    )
-                    .focused(true)
-                    .frame(width: geometry.size.width)
-                    .fixedSize()
-                    .border(Color.red, width: 1)
-                }
-            }
-            Spacer()
+            TextViewRepresentable(
+                text: .constant("Text")
+            )
         }
     }
 }
