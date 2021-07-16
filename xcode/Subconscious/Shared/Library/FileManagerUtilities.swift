8f49b8c8c2e5589f1a3871cd8731fae384956aa3
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-16 12:45:24 -0700

    Add findUniqueFilename
    Will increment filename version until unique

--- xcode/Subconscious/Shared/Library/FileManagerUtilities.swift
+++ xcode/Subconscious/Shared/Library/FileManagerUtilities.swift
@@ -13,18 +13,50 @@ extension FileManager {
         self.urls(for: .documentDirectory, in: .userDomainMask).first
     }
 
-    /// List all URLs for files in the user's document directory.
-    /// Omits hidden files.
-    func listDocumentDirectoryContents() -> [URL] {
-        if let dir = self.documentDirectoryUrl {
-            let files = try? self.contentsOfDirectory(
-                at: dir,
-                includingPropertiesForKeys: nil,
-                options: .skipsHiddenFiles
+    /// Simplified form of `contentsOfDirectory`.
+    func contentsOfDirectory(at url: URL) throws -> [URL] {
+        try self.contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: nil,
+            options: .skipsHiddenFiles
+        )
+    }
+
+    func fileExists(atURL url: URL) -> Bool {
+        self.fileExists(atPath: url.path)
+    }
+
+    /// Get a unique versioned filename for file, given a base URL, name, extension.
+    /// Increments version until it finds a unique filename.
+    /// - Returns: URL
+    func findUniqueFilename(
+        at url: URL,
+        name: String,
+        ext: String,
+        version: Int = 1
+    ) -> URL? {
+        guard url.isFileURL && url.hasDirectoryPath else {
+            return nil
+        }
+
+        // Only version numbers one and above, please.
+        let version = max(version, 1)
+
+        let versionedURL = url.appendingVersionedFilename(
+            name: name,
+            ext: ext,
+            version: version
+        )
+
+        if self.fileExists(atURL: versionedURL) {
+            return findUniqueFilename(
+                at: url,
+                name: name,
+                ext: ext,
+                version: version + 1
             )
-            return files ?? []
         } else {
-            return []
+            return versionedURL
         }
     }
 }
