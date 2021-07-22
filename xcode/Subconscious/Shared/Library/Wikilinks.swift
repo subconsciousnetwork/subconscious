ef45f32ac44783ea69d625189fa17fb9f5a9d30c
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-22 13:53:58 -0700

    Beginnings of Wikilink renderer

--- xcode/Subconscious/Shared/Library/Wikilinks.swift
+++ xcode/Subconscious/Shared/Library/Wikilinks.swift
@@ -6,10 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension String {
+    static let wikilinkPattern = #"\[\[([^\]]+)\]\]"#
+
     /// Find ranges for wikilinks in a string
-    func findWikilinkRanges(_ text: String) throws -> [NSTextCheckingResult] {
-        try text.matches(pattern: #"\[\[([^\]]+)\]\]"#)
+    func findWikilinks(_ text: String) -> [NSTextCheckingResult] {
+        do {
+            return try text.matches(pattern: String.wikilinkPattern)
+        } catch {
+            return []
+        }
+    }
+
+    func renderingWikilinks() -> NSAttributedString {
+        let string = NSMutableAttributedString(string: self)
+        for result in findWikilinks(self) {
+            let range = result.range(at: 1)
+            string.addAttribute(.link, value: "http://google.com", range: range)
+        }
+        return NSAttributedString(attributedString: string)
     }
 }
+
