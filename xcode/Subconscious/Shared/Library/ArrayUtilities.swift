2ed889663b670ae819fa49dbb51cd7e31a1c1df6
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-23 14:38:36 -0700

    Add toDictionary and other helpers

--- xcode/Subconscious/Shared/Library/ArrayUtilities.swift
+++ xcode/Subconscious/Shared/Library/ArrayUtilities.swift
@@ -10,16 +10,27 @@ import Foundation
 extension Sequence where Iterator.Element: Hashable {
     /// Filter unique elements, as determined by hashable.
     /// First element wins.
-    func unique() -> [Iterator.Element] {
+    public func unique() -> [Iterator.Element] {
         var seen: Set<Iterator.Element> = []
         return filter({ item in seen.insert(item).inserted })
+    }
+
+    /// Construct dictionary from array, using key function
+    public func toDictionary<Key: Hashable>(
+        key: (Iterator.Element) -> Key
+    ) -> [Key:Iterator.Element] {
+        var dict: [Key:Iterator.Element] = [:]
+        for element in self {
+            dict[key(element)] = element
+        }
+        return dict
     }
 }
 
 extension Array {
     /// Append two arrays, returning a new array.
     /// Chainable method version of "+" operator.
-    func appending(contentsOf array: [Element]) -> [Element] {
+    public func appending(contentsOf array: [Element]) -> [Element] {
         return self + array
     }
 }
