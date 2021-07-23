ec772651fa6474d5b9a1e58f2236c23ef275a6da
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-23 15:22:21 -0700

    Add wikilinks method to extract wikilinks

--- xcode/Subconscious/Shared/Library/Wikilinks.swift
+++ xcode/Subconscious/Shared/Library/Wikilinks.swift
@@ -11,18 +11,30 @@ import SwiftUI
 extension String {
     static let wikilinkPattern = #"\[\[([^\]]+)\]\]"#
 
+
     /// Find ranges for wikilinks in a string
-    func findWikilinks(_ text: String) -> [NSTextCheckingResult] {
+    func findWikilinks() -> [NSTextCheckingResult] {
         do {
-            return try text.matches(pattern: String.wikilinkPattern)
+            return try self.matches(pattern: String.wikilinkPattern)
         } catch {
             return []
         }
     }
 
+    /// Get an array of Strings, each of which is the text portion of a Wikilink
+    func wikilinks() -> [String] {
+        self.findWikilinks().compactMap({ result in
+            let nsrange = result.range(at: 1)
+            if let range = Range(nsrange, in: self) {
+                return String(self[range])
+            }
+            return nil
+        })
+    }
+
     func renderingWikilinks() -> NSAttributedString {
         let string = NSMutableAttributedString(string: self)
-        for result in findWikilinks(self) {
+        for result in self.findWikilinks() {
             let range = result.range(at: 1)
             string.addAttribute(.link, value: "http://google.com", range: range)
         }
