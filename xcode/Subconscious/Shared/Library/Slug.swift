83b3caec5d64dcff9db228553adcda6be40374e7
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-07-27 13:44:34 -0700

    Convert slug to String extensions

--- xcode/Subconscious/Shared/Library/Slug.swift
+++ xcode/Subconscious/Shared/Library/Slug.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 /// A suite of tools for generating slugs.
-struct Slug {
+extension String {
     /// Truncate to avoid file name length limit issues.
     /// Windows systems can handle up to 255, but we truncate at 200 to leave a bit of room
     /// for things like version numbers.
-    static func truncatingSafeFileNameLength(_ text: String) -> String {
-        String(text.prefix(200))
+    func truncatingSafeFileNameLength() -> String {
+        String(self.prefix(200))
     }
 
     /// Make filename compatible with Mac and FAT file systems by removing forbidden characters.
-    static func sanitizingFilename(_ text: String) -> String {
+    func sanitizingFilename() -> String {
         // Replace slashes and pluses with dashes
-        text
+        self
             .replacingOccurrences(
                 of: #"[\:\/\\\+]"#,
                 with: "-",
@@ -36,14 +36,14 @@ struct Slug {
     }
 
     /// Sanitize and truncate text so that it is suitible as a Mac/FAT filename.
-    static func toFilename(_ text: String) -> String {
-        truncatingSafeFileNameLength(sanitizingFilename(text))
+    func toFilename() -> String {
+        self.sanitizingFilename().truncatingSafeFileNameLength()
     }
 
     /// Replace all runs of one or more whitespace characters
     /// with a single dash
-    static func replacingSpacesWithDash(_ text: String) -> String {
-        text.replacingOccurrences(
+    func replacingSpacesWithDash() -> String {
+        self.replacingOccurrences(
             of: #"\s+"#,
             with: "-",
             options: .regularExpression,
@@ -53,8 +53,8 @@ struct Slug {
 
     /// Remove any character that is not POSIX fully-portable filename.
     /// Basically anything not `[A–Za–z0–9._-]`.
-    static func removingNonPosixCharacters(_ text: String) -> String {
-        text.replacingOccurrences(
+    func removingNonPosixCharacters() -> String {
+        self.replacingOccurrences(
             of: #"[^a-zA-Z0-9\._\-\s]"#,
             with: "",
             options: .regularExpression,
@@ -64,29 +64,12 @@ struct Slug {
 
     /// Given a string, returns a slug suitable for using as a file name.
     /// Slug conforms to POSIX Fully Portable Filename format.
-    static func toSlug(_ text: String) -> String {
-        let trimmed = text.trimmingCharacters(in: .whitespacesAndNewlines)
-        let lower = trimmed.lowercased()
-        let dashed = replacingSpacesWithDash(lower)
-        let portable = removingNonPosixCharacters(dashed)
-        let truncated = truncatingSafeFileNameLength(portable)
-        return truncated
-    }
-
-    /// Given a string, returns a slug prefixed with ISO8601 date.
-    /// Slug conforms to POSIX Fully Portable Filename format.
-    static func toSlugWithDate(_ text: String, date: Date = Date()) -> String {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [
-            .withFullDate,
-            .withDashSeparatorInDate
-        ]
-        let isodate = formatter.string(from: date)
-        let slug = toSlug(text)
-        if slug.count > 0 {
-            return "\(isodate)-\(slug)"
-        } else {
-            return isodate
-        }
+    func toSlug() -> String {
+        self
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .lowercased()
+            .replacingSpacesWithDash()
+            .removingNonPosixCharacters()
+            .truncatingSafeFileNameLength()
     }
 }
