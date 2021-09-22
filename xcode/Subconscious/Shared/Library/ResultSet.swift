37fefc2ece4efb5afb966efe4f655b64f70a1b5a
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-09-22 11:29:45 -0700

    Add ResultSet

---
+++ xcode/Subconscious/Shared/Library/ResultSet.swift
@@ -0,0 +1,13 @@
+//
+//  ResultSet.swift
+//  Subconscious
+//
+//  Created by Gordon Brander on 9/21/21.
+//
+
+import Foundation
+
+struct ResultSet {
+    var entry: SubtextDocumentLocation? = nil
+    var backlinks: [SubtextDocumentLocation] = []
+}
