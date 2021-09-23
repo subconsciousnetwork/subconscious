aab7385ef246a0ef61a15cd3ac05442d26d208f2
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-09-23 10:40:47 -0700

    Visibility tweaks to store

--- xcode/Subconscious/Shared/Library/Store.swift
+++ xcode/Subconscious/Shared/Library/Store.swift
@@ -1,9 +1,28 @@
 //
 //  Store.swift
-//  Subconscious
 //
 //  Created by Gordon Brander on 9/15/21.
 //
+//  MIT LICENSE
+//  Copyright 2021 Gordon Brander
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  allcopies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
 
 import Foundation
 import Combine
@@ -12,14 +31,14 @@ import os
 
 /// An Updatable is a type that knows how create updates and effects in response to actions.
 /// Effects are Combine Publishers that produce Actions and never fail.
-protocol Updatable {
+public protocol Updatable {
     associatedtype Action
     func update(action: Action) -> (Self, AnyPublisher<Action, Never>)
 }
 
-/// Store is a source of truth for a central state.
+/// Store is a source of truth for a state.
 ///
-/// Store is an `ObservableObject`. You use it in a view via `@ObservedObject`
+/// Store is an `ObservableObject`. You can use it in a view via `@ObservedObject`
 /// or `@StateObject` to power view rendering.
 ///
 /// Store has a `@Published` `state` that conforms to`Updatable` (typically a struct).
@@ -27,37 +46,33 @@ protocol Updatable {
 ///
 /// Store is meant to be used as part of a single app-wide, or major-view-wide component.
 /// Store deliberately does not solve for nested components or nested stores. Following Elm,
-/// deeply nested components are discouraged. Instead, the app should use a single store,
-/// or perhaps one store per major view.
+/// deeply nested components are avoided. Instead, an app should use a single store,
+/// or perhaps one store per major view. Components should not have to communicate with
+/// each other. If nested components do have to communicate, it is probably a sign they
+/// should be the same component with a shared store.
 ///
-/// Components should not have to communicate with each other. If nested components do have to
-/// communicate, it is probably a sign they should be the same component with a shared store.
+/// Instead of decomposing an app into components, we decompose the app into views that share the
+/// same store and actions. Sub-views should be either stateless, consuming bare properties
+/// of `store.state`, or take bindings, which can be created with `store.binding`.
 ///
-/// Instead of decomposing an app into components, we decompose the app into sub-views that share the
-/// same store and actions. This cuts down on the amount of mapping and unwrapping that must be done.
-/// It also means we don't need machinery for republishing changes across nested stores, and we don't
-/// need to hold on to redundant state across multiple components.
-///
-/// Sub-views should be either stateless, consuming bare properties of `store.state`, or
-/// take bindings, which can be created with `store.binding`.
-///
-/// See https://guide.elm-lang.org/webapps/structure.html
-/// for more about this philosophy.
-final class Store<Model>: ObservableObject
-where Model: Updatable
+/// See https://guide.elm-lang.org/architecture/
+/// and https://guide.elm-lang.org/webapps/structure.html
+/// for more about this approach.
+public final class Store<State>: ObservableObject
+where State: Updatable
 {
     /// Logger, used when in debug mode
-    private(set) var logger: Logger
+    private var logger: Logger
     /// Toggle debug mode
-    private(set) var debug: Bool
-    /// Holds on to cancellables until they complete
-    private var cancellables = PublisherBag()
+    public private(set) var debug: Bool
+    /// Stores cancellables by ID
+    private var cancellables: [UUID: AnyCancellable] = [:]
     /// Current state.
     /// All writes to state happen through actions sent to `Store.send`.
-    @Published private(set) var state: Model
+    @Published public private(set) var state: State
 
     init(
-        state: Model,
+        state: State,
         logger: Logger,
         debug: Bool = false
     ) {
@@ -66,11 +81,11 @@ where Model: Updatable
         self.debug = debug
     }
 
-    /// Create a binding for updating the store somehow.
-    /// Updates are sent via actions, rather than setting values directly.
-    func binding<Value>(
-        get: @escaping (Model) -> Value,
-        tag: @escaping (Value) -> Model.Action
+    /// Create a binding that can update the store.
+    /// Sets send actions to the store, rather than setting values directly.
+    public func binding<Value>(
+        get: @escaping (State) -> Value,
+        tag: @escaping (Value) -> State.Action
     ) -> Binding<Value> {
         Binding(
             get: { get(self.state) },
@@ -82,7 +97,7 @@ where Model: Updatable
 
     /// Send an action to the store to update state and generate effects.
     /// Any effects generated are fed back into the store.
-    func send(action: Model.Action) {
+    public func send(action: State.Action) {
         // Generate next state and effect
         let (next, effect) = state.update(action: action)
         if debug {
@@ -103,12 +118,35 @@ where Model: Updatable
         // Set state. This mutates published property, firing objectWillChange.
         self.state = next
         // Run effect
-        cancellables.sink(
+        sink(
             publisher: effect.receive(
                 on: DispatchQueue.main,
                 options: .init(qos: .userInitiated)
             ).eraseToAnyPublisher(),
             receiveValue: self.send
         )
+    }
+
+    /// Subscribe to a publisher until it completes.
+    /// Retains the cancellable reference until the cancellable is complete.
+    private func sink(
+        publisher: AnyPublisher<State.Action, Never>,
+        receiveValue: @escaping (State.Action) -> Void
+    ) {
+        // Create a UUID for the cancellable.
+        // Store cancellable in dictionary by UUID.
+        // Remove cancellable from dictionary upon effect completion.
+        // This retains the effect pipeline for as long as it takes to complete
+        // the effect, and then removes it, so we don't have a cancellables
+        // memory leak.
+        let id = UUID()
+        let cancellable = publisher
+            .sink(
+                receiveCompletion: { [weak self] _ in
+                    self?.cancellables.removeValue(forKey: id)
+                },
+                receiveValue: receiveValue
+            )
+        self.cancellables[id] = cancellable
     }
 }
