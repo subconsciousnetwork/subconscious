6687ef70ea2ffe6422e736df217a72fb092ae37e
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-10-07 15:04:11 -0700

    Remove methods no longer needed

--- xcode/Subconscious/Shared/Library/URLUtilities.swift
+++ xcode/Subconscious/Shared/Library/URLUtilities.swift
@@ -22,27 +22,6 @@ extension URL {
         return url
     }
 
-    /// Add file name component to URL with version and extension.
-    /// Useful for uniqueing file names.
-    /// - Returns: new URL
-    func appendingVersionedFilename(
-        name: String,
-        ext: String,
-        version: Int = 1,
-        untitled: String = "Untitled"
-    ) -> URL {
-        let trimmed = name.trimmingCharacters(in: .whitespacesAndNewlines)
-        let name = trimmed.isEmpty ? untitled : name
-        var url = self
-        // Only version numbers one and above, please.
-        let version = max(version, 1)
-        // Only append version number if > 0.
-        let versionedName = version == 1 ? name : "\(name) \(version)"
-        url.appendPathComponent(versionedName, isDirectory: false)
-        url.appendPathExtension(ext)
-        return url
-    }
-
     /// Return path relative to some base
     /// If URL does not start with base, returns nil.
     func relativizingPath(relativeTo base: URL) -> String? {
