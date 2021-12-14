4ff9cc502a3bd3760a27fc91fef890044bbdba04
Gordon Brander <gordonbrander@users.noreply.github.com>
2021-12-14 12:28:52 -0800

    Add FABButtonStyle

--- xcode/Subconscious/Shared/Components/Common/FABButtonStyle.swift
+++ xcode/Subconscious/Shared/Components/Common/FABButtonStyle.swift
@@ -5,4 +5,36 @@
 //  Created by Gordon Brander on 12/14/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct FABButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            Circle()
+                .foregroundColor(
+                    configuration.isPressed ?
+                    Color.fabBackgroundPressed :
+                    Color.fabBackground
+                )
+                .frame(
+                    width: 56,
+                    height: 56,
+                    alignment: .center
+                )
+                .shadow(
+                    radius: 8,
+                    x: 0,
+                    y: 4
+                )
+            configuration.label
+                .foregroundColor(
+                    isEnabled ? Color.fabText : Color.fabTextDisabled
+                )
+                .contentShape(
+                    Circle()
+                )
+        }
+    }
+}
