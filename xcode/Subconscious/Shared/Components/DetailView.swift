85cdc37bba8170187fe84c51bcf98440882850d3
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-01-12 13:09:45 -0800

    Stub in custom back button
    Making DetailView top-level within the NavigationLink destination resolves
    some fade/animation issues with NavigationView transition.
    
    Move NavigationView settings into DetailView. Moving these properties
    into DetailView resolves some animation issues. It also makes the view
    more modular, since default back button is disabled and custom back
    button inserted in same view.
    
    Move ProgressView into DetailView. This enables detail view to be
    top-level with the NavigationLink destination.

--- xcode/Subconscious/Shared/Components/DetailView.swift
+++ xcode/Subconscious/Shared/Components/DetailView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct DetailView: View {
+    /// Access dismiss function from environment. This lets us drive the custom
+    /// back button behavior.
+    /// See https://developer.apple.com/documentation/swiftui/dismissaction.
+    /// 2021-01-12 Gordon Brander
+    @Environment(\.dismiss) var dismiss
+    /// If we have an entryURL, we're ready to edit.
+    /// If we don't, we have nothing to edit.
+    var entryURL: URL?
+    var backlinks: [EntryStub]
     @Binding var focus: AppModel.Focus?
     @Binding var editorAttributedText: NSAttributedString
     @Binding var editorSelection: NSRange
     @Binding var isLinkSheetPresented: Bool
     @Binding var linkSearchText: String
     @Binding var linkSuggestions: [Suggestion]
-    var backlinks: [EntryStub]
     var onDone: () -> Void
     var onEditorLink: (
         URL,
@@ -26,49 +34,67 @@ struct DetailView: View {
     var onCommitLinkSearch: (String) -> Void
 
     var body: some View {
-        GeometryReader { geometry in
-            PseudoKeyboardToolbarView(
-                isKeyboardUp: focus == .editor,
-                toolbarHeight: 48,
-                toolbar: KeyboardToolbarView(
-                    isSheetPresented: $isLinkSheetPresented,
-                    suggestions: .constant([])
-                ),
-                content: { isKeyboardUp, size in
-                    ScrollView(.vertical) {
-                        VStack(spacing: 0) {
-                            GrowableAttributedTextViewRepresentable(
-                                attributedText: $editorAttributedText,
-                                selection: $editorSelection,
-                                focus: $focus,
-                                field: .editor,
-                                onLink: onEditorLink,
-                                fixedWidth: size.width
-                            )
-                            .insets(
-                                EdgeInsets(
-                                    top: AppTheme.margin,
-                                    leading: AppTheme.margin,
-                                    bottom: AppTheme.margin,
-                                    trailing: AppTheme.margin
-                                )
-                            )
-                            .frame(
-                                minHeight: size.height
-                            )
-                            if !isKeyboardUp && backlinks.count > 0 {
-                                Divider()
-                                BacklinksView(
-                                    backlinks: backlinks,
-                                    onActivateBacklink: onCommitSearch
-                                )
+        VStack {
+            if entryURL == nil {
+                ProgressScrim()
+            } else {
+                GeometryReader { geometry in
+                    PseudoKeyboardToolbarView(
+                        isKeyboardUp: focus == .editor,
+                        toolbarHeight: 48,
+                        toolbar: KeyboardToolbarView(
+                            isSheetPresented: $isLinkSheetPresented,
+                            suggestions: .constant([])
+                        ),
+                        content: { isKeyboardUp, size in
+                            ScrollView(.vertical) {
+                                VStack(spacing: 0) {
+                                    GrowableAttributedTextViewRepresentable(
+                                        attributedText: $editorAttributedText,
+                                        selection: $editorSelection,
+                                        focus: $focus,
+                                        field: .editor,
+                                        onLink: onEditorLink,
+                                        fixedWidth: size.width
+                                    )
+                                    .insets(
+                                        EdgeInsets(
+                                            top: AppTheme.margin,
+                                            leading: AppTheme.margin,
+                                            bottom: AppTheme.margin,
+                                            trailing: AppTheme.margin
+                                        )
+                                    )
+                                    .frame(
+                                        minHeight: size.height
+                                    )
+                                    if !isKeyboardUp && backlinks.count > 0 {
+                                        Divider()
+                                        BacklinksView(
+                                            backlinks: backlinks,
+                                            onActivateBacklink: onCommitSearch
+                                        )
+                                    }
+                                }
                             }
                         }
-                    }
+                    )
                 }
-            )
+            }
         }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
         .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(
+                    action: {
+                        dismiss()
+                    }
+                ) {
+                    Text("Back")
+                }
+            }
             ToolbarItem(placement: .confirmationAction) {
                 if focus == .editor {
                     Button(
