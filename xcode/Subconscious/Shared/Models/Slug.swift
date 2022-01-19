411f9ea0fa844109fc51eaa3f7116f4b3be649d9
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-01-19 14:14:38 -0800

    Add slugification methods to typealias

--- xcode/Subconscious/Shared/Models/Slug.swift
+++ xcode/Subconscious/Shared/Models/Slug.swift
@@ -7,11 +7,41 @@
 
 import Foundation
 
+
 /// A slug is a local path and ID for an entry
 /// Currently this is just a typealias. In future we may give it a
 /// distinct type.
 typealias Slug = String
 
+extension String {
+    /// Slugify a string, returning a slug.
+    func slugify() -> Slug {
+        self
+            .lowercased()
+            // Replace runs of one or more space with a single dash
+            .replacingOccurrences(
+                of: #"\s+"#,
+                with: "-",
+                options: .regularExpression,
+                range: nil
+            )
+            // Remove all non-slug characters
+            .replacingOccurrences(
+                of: #"[^a-zA-Z0-9_\-\/]"#,
+                with: "",
+                options: .regularExpression,
+                range: nil
+            )
+            .truncatingSafeFileNameLength()
+            .ltrim(prefix: "/")
+    }
+
+    /// Slugify a string, returning a string.
+    /// For now, this is just a proxy to toSlug.
+    func slugifyString() -> String {
+        self.slugify()
+    }
+}
 
 extension URL {
     /// Convert URL to slug
