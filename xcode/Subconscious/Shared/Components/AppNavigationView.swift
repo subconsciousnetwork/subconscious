2437f0ad3d52e9ba80b20e85eca6b71dca10ee09
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-02-10 13:24:45 -0800

    Do not show recents when zero elements
    This causes transition (opacity) to play instead of animation
    (grow/shrink). Important since the results may appear after onAppear,
    given the fetch effect is asyncronous.

--- xcode/Subconscious/Shared/Components/AppNavigationView.swift
+++ xcode/Subconscious/Shared/Components/AppNavigationView.swift
@@ -13,58 +13,60 @@ struct AppNavigationView: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-                List(store.state.recent) { entry in
-                    Button(
-                        action: {
-                            store.send(
-                                action: .requestDetail(
-                                    slug: entry.slug,
-                                    fallback: entry.title
-                                )
-                            )
-                        }
-                    ) {
-                        EntryRow(entry: entry)
-                            .padding(.vertical, AppTheme.unit2)
-                    }
-                    .swipeActions(
-                        edge: .trailing,
-                        allowsFullSwipe: false
-                    ) {
+                if store.state.recent.count > 0 {
+                    List(store.state.recent) { entry in
                         Button(
-                            role: .destructive,
                             action: {
                                 store.send(
-                                    action: .confirmDelete(entry.slug)
+                                    action: .requestDetail(
+                                        slug: entry.slug,
+                                        fallback: entry.title
+                                    )
                                 )
                             }
                         ) {
-                            Text("Delete")
+                            EntryRow(entry: entry)
+                                .padding(.vertical, AppTheme.unit2)
                         }
-                    }
-                }
-                .transition(.opacity)
-                .animation(.easeOutCubic(), value: store.state.recent)
-                .listStyle(.plain)
-                .confirmationDialog(
-                    "Are you sure?",
-                    isPresented: store.binding(
-                        get: \.isConfirmDeleteShowing,
-                        tag: AppAction.setConfirmDeleteShowing
-                    ),
-                    presenting: store.state.entryToDelete
-                ) { slug in
-                    Button(
-                        role: .destructive,
-                        action: {
-                            withAnimation {
-                                store.send(
-                                    action: .deleteEntry(slug)
-                                )
+                        .swipeActions(
+                            edge: .trailing,
+                            allowsFullSwipe: false
+                        ) {
+                            Button(
+                                role: .destructive,
+                                action: {
+                                    store.send(
+                                        action: .confirmDelete(entry.slug)
+                                    )
+                                }
+                            ) {
+                                Text("Delete")
                             }
                         }
-                    ) {
-                        Text("Delete Immediately")
+                    }
+                    .animation(.easeOutCubic(), value: store.state.recent)
+                    .transition(.opacity)
+                    .listStyle(.plain)
+                    .confirmationDialog(
+                        "Are you sure?",
+                        isPresented: store.binding(
+                            get: \.isConfirmDeleteShowing,
+                            tag: AppAction.setConfirmDeleteShowing
+                        ),
+                        presenting: store.state.entryToDelete
+                    ) { slug in
+                        Button(
+                            role: .destructive,
+                            action: {
+                                withAnimation {
+                                    store.send(
+                                        action: .deleteEntry(slug)
+                                    )
+                                }
+                            }
+                        ) {
+                            Text("Delete Immediately")
+                        }
                     }
                 }
                 NavigationLink(
