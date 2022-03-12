e7ca36a8e80507f1aee0bd0cba3a382eed54a56e
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-03-11 18:03:02 -0800

    Add Unit Test target
    ...and stub out Slug tests.
    
    It turns out that the tests which XCode stubs out for you in the XCode
    SwiftUI project builder are UI Tests, which are a completely different
    kind of target than Unit Tests. To write Unit Tests you must create a
    new Unit Test target in the project definition.
    
    I've done that here. Added a SubconsciousTests target which is a Unit
    Tests target.

---
+++ xcode/Subconscious/SubconsciousTests/SubconsciousTests.swift
@@ -0,0 +1,21 @@
+//
+//  SubconsciousTests.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 3/11/22.
+//
+
+import XCTest
+@testable import Subconscious
+
+class SubconsciousTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
