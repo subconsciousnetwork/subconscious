84b00dd915fc5669649fba189bc9f0c72e2a3fcc
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-04-22 12:16:45 -0700

    Always show backlinks (#292)
    Provide a hint when there are no backlinks to show

--- xcode/Subconscious/Shared/Components/BacklinksView.swift
+++ xcode/Subconscious/Shared/Components/BacklinksView.swift
@@ -14,30 +14,41 @@ struct BacklinksView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             HStack {
-                Text(
-                    "Backlinks"
-                )
+                Text("Backlinks")
                 .font(Font(UIFont.appCaption))
                 Spacer()
             }
             .padding(.horizontal, AppTheme.unit4)
             .padding(.vertical, AppTheme.unit2)
-            ForEach(backlinks) { entry in
-                Divider()
-                Button(
-                    action: {
-                        onSelect(
-                            EntryLink(
-                                slug: entry.slug,
-                                title: entry.title
+            if backlinks.count > 0 {
+                ForEach(backlinks) { entry in
+                    Divider()
+                    Button(
+                        action: {
+                            onSelect(
+                                EntryLink(
+                                    slug: entry.slug,
+                                    title: entry.title
+                                )
                             )
-                        )
-                    },
-                    label: {
-                        EntryRow(entry: entry)
-                    }
+                        },
+                        label: {
+                            EntryRow(entry: entry)
+                        }
+                    )
+                    .buttonStyle(BacklinkButtonStyle())
+                }
+            } else {
+                Divider()
+                TitleGroupView(
+                    title: Text("No backlinks yet")
+                        .foregroundColor(Color.secondaryText),
+                    subtitle: Text(
+                        "Links to this note will appear here"
+                    )
                 )
-                .buttonStyle(BacklinkButtonStyle())
+                .padding(.horizontal, AppTheme.unit4)
+                .padding(.vertical, AppTheme.unit3)
             }
         }
     }
@@ -45,16 +56,22 @@ struct BacklinksView: View {
 
 struct BacklinksView_Previews: PreviewProvider {
     static var previews: some View {
-        BacklinksView(
-            backlinks: [
-                EntryStub(
-                    slug: Slug("floop")!,
-                    title: "Floop",
-                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
-                    modified: Date.now
-                )
-            ],
-            onSelect: { title in }
-        )
+        VStack {
+            BacklinksView(
+                backlinks: [
+                    EntryStub(
+                        slug: Slug("floop")!,
+                        title: "Floop",
+                        excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.",
+                        modified: Date.now
+                    )
+                ],
+                onSelect: { title in }
+            )
+            BacklinksView(
+                backlinks: [],
+                onSelect: { title in }
+            )
+        }
     }
 }
