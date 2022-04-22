3e25257e811f40597ec01ee33abc07b9918bbc82
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-04-22 15:25:42 -0700

    Stop parser when encountering first empty line

---
+++ xcode/Subconscious/Shared/Library/HeaderParser.swift
@@ -0,0 +1,173 @@
+//
+//  HeaderParser.swift
+//  Subconscious (iOS)
+//
+//  Created by Gordon Brander on 4/18/22.
+//
+
+import Foundation
+
+struct HeaderParser {
+    struct Header {
+        var name: Substring
+        var value: Substring
+    }
+
+    /// Fast-forward to the end of the line
+    private static func advanceToEndOfLine(
+        tape: inout Tape<String>
+    ) {
+        while !tape.isExhausted() {
+            let curr = tape.consume()
+            if curr == "\n" {
+                return
+            }
+        }
+    }
+
+    /// Advance past contiguous whitespace.
+    /// Useful for moving past a range of space.
+    private static func advancePastSpaces(
+        tape: inout Tape<String>
+    ) {
+        while !tape.isExhausted() {
+            let next = tape.peek()
+            if next == " " {
+                tape.advance()
+            } else {
+                return
+            }
+        }
+    }
+
+    private static func parseHeaderKey(
+        tape: inout Tape<String>
+    ) -> Substring? {
+        tape.start()
+        while !tape.isExhausted() {
+            let next = tape.peek()!
+            // If end of key, cut tape and return key
+            if next == ":" {
+                // Cut tape, getting key value
+                let key = tape.cut()
+                // Advance tape, discarding `:`
+                tape.advance()
+                return key
+            }
+            // Invalid! Header keys cannot contain spaces.
+            // Throw away the rest of the line and return nil.
+            else if next == " " {
+                advanceToEndOfLine(tape: &tape)
+                return nil
+            }
+            // Invalid! This header has no key delimiter.
+            // Throw away the rest of the line and return nil.
+            else if next == "\n" {
+                advanceToEndOfLine(tape: &tape)
+                return nil
+            }
+            // Invalid! HTTP header keys must be ASCII
+            else if !next.isASCII {
+                advanceToEndOfLine(tape: &tape)
+                return nil
+            }
+            // Character is part of key. Advance tape so it is part of our
+            // tape range.
+            else {
+                tape.advance()
+            }
+        }
+        // If we got to the end of the file without finding a valid header key,
+        // return nil.
+        return nil
+    }
+
+    /// Whitespace before the value is ignored.
+    private static func parseHeaderValue(
+        tape: inout Tape<String>
+    ) -> Substring? {
+        // Discard leading space, per spec
+        advancePastSpaces(tape: &tape)
+        // Mark tape start
+        tape.start()
+        while !tape.isExhausted() {
+            let next = tape.peek()
+            if next == "\n" {
+                let value = tape.cut()
+                // Throw away newline
+                tape.advance()
+                return value
+            } else {
+                tape.advance()
+            }
+        }
+        return nil
+    }
+
+    /// Parse a single line from tape, returning it
+    private static func parseHeader(tape: inout Tape<String>) -> Header? {
+        guard let key = parseHeaderKey(tape: &tape) else {
+            return nil
+        }
+        guard let value = parseHeaderValue(tape: &tape) else {
+            return nil
+        }
+        return Header(name: key, value: value)
+    }
+
+    /// Splits lines in markup, keeping line endings
+    static func parse(_ text: String) -> Self {
+        var tape = Tape(text)
+        var headers: [Header] = []
+        // Sniff first line for header. If it can't be parsed as a header, stop.
+        // We consider this a document without headers.
+        guard let header = parseHeader(tape: &tape) else {
+            // Return empty header struct
+            return Self(
+                text: tape.collection,
+                headerText: tape.collection[text.startIndex..<text.startIndex],
+                headers: []
+            )
+        }
+        headers.append(header)
+        while !tape.isExhausted() {
+            let next = tape.peek()
+            /// First empty line ends header parsing
+            if next != nil && next!.isNewline {
+                return Self(
+                    text: tape.collection,
+                    headerText: tape.collection[
+                        tape.collection.startIndex..<tape.currentIndex
+                    ],
+                    headers: headers
+                )
+            } else if let header = parseHeader(tape: &tape) {
+                headers.append(header)
+            }
+        }
+        return Self(
+            text: tape.collection,
+            headerText: tape.collection[
+                tape.collection.startIndex..<tape.currentIndex
+            ],
+            headers: headers
+        )
+    }
+
+    let text: String
+    let headerText: Substring
+    let headers: [Header]
+
+    /// Call `HeaderParser.parse` to get an instance.
+    /// Initializer is private, since all of these fields need to be
+    /// constructed via a parse.
+    private init(
+        text: String,
+        headerText: Substring,
+        headers: [Header]
+    ) {
+        self.text = text
+        self.headerText = headerText
+        self.headers = headers
+    }
+}
---
+++ xcode/Subconscious/SubconsciousTests/Tests_HeaderParser.swift
@@ -0,0 +1,178 @@
+//
+//  Tests_HeaderParser.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 4/18/22.
+//
+
+import XCTest
+@testable import Subconscious
+
+class Tests_HeaderParser: XCTestCase {
+    func testStopsParsingAtFirstEmptyLine() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title: Floop the Pig
+        Date: 2022-03-18
+
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+
+        XCTAssertEqual(
+            headers.headers.count,
+            3,
+            "Stops parsing headers at first empty line"
+        )
+
+        XCTAssertEqual(
+            String(headers.headerText),
+            "Content-Type: text/subtext\nTitle: Floop the Pig\nDate: 2022-03-18\n",
+            "Stops parsing headers at first empty line"
+        )
+    }
+
+    func testStopsBeforeHeaderlikeContentInBody() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title: Floop the Pig
+        
+        Body: content
+        """
+        let headers = HeaderParser.parse(markup)
+        XCTAssertEqual(
+            headers.headers.count,
+            2,
+            "Stops parsing after first blank line"
+        )
+    }
+
+    func testHeaders() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title: Floop the Pig
+        
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+        XCTAssertEqual(
+            String(headers.headers[0].name),
+            "Content-Type"
+        )
+    }
+
+    func testMoreHeaders() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title: Floop the Pig
+        Accept: */*
+        Cache-Control: no-cache
+        Content-Length: 438
+        Vary: Accept-Encoding,User-Agent
+        Connection: close
+        Date: Thu, 08 Sep 2011 08:57:00 GMT
+        Server: Apache
+        
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+        XCTAssertEqual(
+            String(headers.headers[0].name),
+            "Content-Type"
+        )
+    }
+
+    func testValueOmitsNewline() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title : Floop the Pig
+        
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+
+        XCTAssertEqual(
+            String(headers.headers[0].value),
+            "text/subtext",
+            "Value omits leading spaces and trailing newline"
+        )
+    }
+
+    func testSniffsFirstLine() throws {
+        let markup = """
+        Not a header
+        Content-Type: text/subtext
+        Title : Floop the Pig
+
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+        XCTAssertEqual(
+            headers.headers.count,
+            0,
+            "Sniffs first line to see if it should expect headers"
+        )
+    }
+
+    func testRejectsKeysWithSpaces() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title : Floop the Pig
+        
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+
+        XCTAssertEqual(
+            headers.headers.count,
+            1,
+            "Rejects headers that have spaces in keys"
+        )
+    }
+
+    func testRejectsKeysWithSpaces2() throws {
+        let markup = """
+        Content-Type: text/subtext
+        Title: Floop the Pig
+        Accept: */*
+        Cache-Control: no-cache
+        Key with space: some text
+        Content-Length: 438
+        Vary: Accept-Encoding,User-Agent
+        Connection: close
+        Date: Thu, 08 Sep 2011 08:57:00 GMT
+        Server: Apache
+        
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+
+        XCTAssertEqual(
+            headers.headers.count,
+            9,
+            "Rejects headers that have spaces in keys"
+        )
+
+        XCTAssertEqual(
+            headers.headers[4].name,
+            "Content-Length",
+            "Rejects headers that have spaces in keys"
+        )
+    }
+
+    func testRejectsNonAsciiKeys() throws {
+        let markup = """
+        Title: Floop the Pig
+        Non-Ascii-Header-Key-😈: mwhahaha
+
+        Body content
+        """
+        let headers = HeaderParser.parse(markup)
+
+        XCTAssertEqual(
+            headers.headers.count,
+            1,
+            "Rejects headers that have non-ascii characters in keys"
+        )
+    }
+}
