subconsciousnetwork/subconscious

commit 39fffeffd60fc658ace8170fa813ec1dd06b6070

Fix slashlink parsing after multiple spaces (#294)
We neglected to backtrack in the previous implementation, leading to
more than one space being discarded in some cases. This meant that an
even number of leading spaces would cause slashlinks not to be detected.

This fix backtracks if inlineWordBoundaryForm returns nil, fixing the
problem.

Added tests as well.

xcode/Subconscious/Shared/Library/Subtext.swift
@@ -147,7 +147,9 @@ struct Subtext: Hashable, Equatable {
     }
 
     /// Consume a well-formed bracket link, or else backtrack
-    private static func consumeBracketLink(tape: inout Tape<Substring>) -> Substring? {
+    private static func consumeBracketLink(
+        tape: inout Tape<Substring>
+    ) -> Substring? {
         tape.save()
         while !tape.isExhausted() {
             if tape.consumeMatch(" ") {
@@ -376,9 +378,9 @@ struct Subtext: Hashable, Equatable {
         if tape.consumeMatch("/") {
             let span = consumeSlashlinkBody(tape: &tape)
             return .slashlink(Slashlink(span: span))
-        } else {
-            return nil
         }
+        tape.backtrack()
+        return nil
     }
 
     /// Consume inline forms that are not sensitive to word boundaries

xcode/Subconscious/SubconsciousTests/Tests_Subtext.swift
@@ -284,6 +284,38 @@ class Tests_Subtext: XCTestCase {
         )
     }
 
+    func testSlashlinkParsingTwoSpaces() throws {
+        let markup = "Some text with a  /slashlink that has two spaces preceding."
+        let dom = Subtext(markup: markup)
+
+        guard case let .slashlink(slashlink) = dom.blocks[0].inline.get(0) else {
+            XCTFail("Expected slashlink")
+            return
+        }
+
+        XCTAssertEqual(
+            String(describing: slashlink),
+            "/slashlink",
+            "Slashlink parses successfully"
+        )
+    }
+
+    func testSlashlinkParsingThreeSpaces() throws {
+        let markup = "Some text with a   /slashlink that has three spaces preceding."
+        let dom = Subtext(markup: markup)
+
+        guard case let .slashlink(slashlink) = dom.blocks[0].inline.get(0) else {
+            XCTFail("Expected slashlink")
+            return
+        }
+
+        XCTAssertEqual(
+            String(describing: slashlink),
+            "/slashlink",
+            "Slashlink parses successfully"
+        )
+    }
+
     func testWikilinkParsing0() throws {
         let markup = """
         Let's test out some [[wikilinks]].

commit cab43677df13835b5adb4b54beeacff7fd755e57

Fix slashlink parsing leading space (#297)
Followup to #295 and #294. Third time is the charm?

#295 fixed the infinite loop introduced by #294. However, it caused some tests to fail because it matched the leading space within the slashlink. This still worked from the user perspective, but it meant that the parsed tag included a leading space (wrong).

This PR fixes the issue with a well-placed `tape.start()`. It also simplifies the line breaking code using Swift's split feature, introducing a few benefits:

- Handles all linebreak types
- Drops trailing `\n` making block code simpler

In addition, blocks no longer assume their range includes newlines. New tests ensure the parser does not include them in block spans.

xcode/Subconscious/Shared/Library/StringUtilities.swift
@@ -30,16 +30,6 @@ extension String {
     }
 }
 
-extension String {
-    func splitlines() -> [Substring] {
-        self.split(
-            maxSplits: Int.max,
-            omittingEmptySubsequences: true,
-            whereSeparator: \.isNewline
-        )
-    }
-}
-
 extension StringProtocol {
     /// Check if a string contains only whitespace characters
     var isWhitespace: Bool {

xcode/Subconscious/Shared/Library/Subtext.swift
@@ -29,11 +29,11 @@ struct Subtext: Hashable, Equatable {
         func body() -> Substring {
             switch self {
             case .text(let span, _):
-                return span.trimming(" ").trimming("\n")
+                return span.trimming(" ")
             case .empty(let span):
                 return span
             case .quote(let span, _), .list(let span, _), .heading(let span):
-                return span.dropFirst().trimming(" ").trimming("\n")
+                return span.dropFirst().trimming(" ")
             }
         }
     }
@@ -361,20 +361,14 @@ struct Subtext: Hashable, Equatable {
         return tape.cut()
     }
 
-    /// Consume one of the inline forms that is delimited by word boundaries.
+    /// Consume inline form after identifying a word boundary.
     /// A word boundary is generally a preceding space, or beginning of input.
-    /// Cuts tape before word boundary.
     /// - Returns: Inline?
     private static func consumeInlineWordBoundaryForm(
         tape: inout Tape<Substring>
     ) -> Inline? {
-        let isAtStart = tape.currentIndex == tape.collection.startIndex
-        // Inline word boundary forms must appear at beginning of block
-        // or after a space.
-        guard isAtStart || tape.consumeMatch(" ") else {
-            return nil
-        }
         tape.save()
+        tape.start()
         if tape.consumeMatch("/") {
             let span = consumeSlashlinkBody(tape: &tape)
             return .slashlink(Slashlink(span: span))
@@ -433,11 +427,16 @@ struct Subtext: Hashable, Equatable {
     /// - Returns: an array of inline forms
     private static func parseInline(tape: inout Tape<Substring>) -> [Inline] {
         var inlines: [Inline] = []
-
+        /// Check for inline word boundary forms at beginning of input.
+        if let inline = consumeInlineWordBoundaryForm(tape: &tape) {
+            inlines.append(inline)
+        }
         while !tape.isExhausted() {
             tape.start()
-            if let inline = consumeInlineWordBoundaryForm(tape: &tape) {
-                inlines.append(inline)
+            if tape.consumeMatch(" ") {
+                if let inline = consumeInlineWordBoundaryForm(tape: &tape) {
+                    inlines.append(inline)
+                }
             } else if let inline = consumeInlineForm(tape: &tape) {
                 inlines.append(inline)
             } else {
@@ -463,7 +462,7 @@ struct Subtext: Hashable, Equatable {
             tape.advance()
             let inline = parseInline(tape: &tape)
             return Block.list(span: line, inline: inline)
-        } else if line == "\n" {
+        } else if line == "" {
             return Block.empty(span: line)
         } else {
             var tape = Tape(line)
@@ -472,27 +471,13 @@ struct Subtext: Hashable, Equatable {
         }
     }
 
-    /// Parse a single line from tape, returning it
-    private static func consumeLine(tape: inout Tape<String>) -> Substring {
-        tape.start()
-        while !tape.isExhausted() {
-            let curr = tape.consume()
-            if curr == "\n" {
-                return tape.cut()
-            }
-        }
-        return tape.cut()
-    }
-
     /// Splits lines in markup, keeping line endings
     private static func parseLines(_ string: String) -> [Substring] {
-        var tape = Tape(string)
-        var lines: [Substring] = []
-        while !tape.isExhausted() {
-            let line = consumeLine(tape: &tape)
-            lines.append(line)
-        }
-        return lines
+        string.split(
+            maxSplits: Int.max,
+            omittingEmptySubsequences: false,
+            whereSeparator: \.isNewline
+        )
     }
 
     let base: String

xcode/Subconscious/SubconsciousTests/Tests_Subtext.swift
@@ -983,4 +983,52 @@ class Tests_Subtext: XCTestCase {
             "Finds wikilink when cursor is at end of text"
         )
     }
+
+    func testBlockDoesNotEndInLinebreak() throws {
+        let markup = """
+        
+        Some text after an empty block
+        # A heading block
+        - A list block [[with a wikilink]]
+        > A quote block /with-slashlink
+        """
+        let dom = Subtext(markup: markup)
+
+        guard case .empty(let empty) = dom.blocks[0] else {
+            XCTFail("Expected empty")
+            return
+        }
+        XCTAssertEqual(empty.count, 0)
+        XCTAssertEqual(empty.first, nil)
+        XCTAssertEqual(empty.last, nil)
+
+        guard case .text(let text, _) = dom.blocks[1] else {
+            XCTFail("Expected text")
+            return
+        }
+        XCTAssertEqual(text.first, "S")
+        XCTAssertEqual(text.last, "k")
+
+        guard case .heading(let heading) = dom.blocks[2] else {
+            XCTFail("Expected heading")
+            return
+        }
+        XCTAssertEqual(heading.first, "#")
+        XCTAssertEqual(heading.last, "k")
+
+        guard case .list(let list, _) = dom.blocks[3] else {
+            XCTFail("Expected list")
+            return
+        }
+        XCTAssertEqual(list.first, "-")
+        XCTAssertEqual(list.last, "]")
+
+        guard case .quote(let quote, _) = dom.blocks[4] else {
+            XCTFail("Expected quote")
+            return
+        }
+        XCTAssertEqual(quote.first, ">")
+        XCTAssertEqual(quote.last, "k")
+
+    }
 }