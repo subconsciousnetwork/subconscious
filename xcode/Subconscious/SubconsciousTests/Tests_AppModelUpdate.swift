b97effe7cd1402716d86f1849d68a6ad78f4a897
Naginat4 <103612133+Naginat4@users.noreply.github.com>
2022-05-03 11:49:20 -0700

    Add tests for some update fns (#307)
    * Add tests for some update fns
    
    Add basic state tests for
    
    - entryCount
    - deleteEntry
    - showRenameSheet
    
    * Update Tests_AppModelUpdate.swift
    
    Added a test for HideRenameSheet
    
    * Update Tests_AppModelUpdate.swift
    
    New test for Rename Slug Field
    
    * Update Tests_AppModelUpdate.swift
    
    Wrote test for
    setSearch
    
    * Update Tests_AppModelUpdate.swift
    
    Add tests for hideSearch, and additional tests for updateDetail
    
    * Update Tests_AppModelUpdate.swift
    
    Add tests for save

--- xcode/Subconscious/SubconsciousTests/Tests_AppModelUpdate.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_AppModelUpdate.swift
@@ -33,6 +33,16 @@ class Tests_AppModelUpdate: XCTestCase {
             environment: environment
         )
         XCTAssertEqual(
+            update.state.isDetailLoading,
+            false,
+            "isDetailLoading set to false"
+        )
+        XCTAssertEqual(
+            update.state.slug,
+            detail.slug,
+            "Sets the slug"
+        )
+        XCTAssertEqual(
             update.state.editorText,
             "Example text",
             "Sets editor text"
@@ -92,6 +102,194 @@ class Tests_AppModelUpdate: XCTestCase {
             update.state.focus,
             nil,
             "Autofocus sets editor focus"
+        )
+    }
+
+    func testEntryCount() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .setEntryCount(10),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.entryCount,
+            10,
+            "Entry count correctly set"
+        )
+    }
+
+    func testDeleteEntry() throws {
+        let slugA = Slug("a")!
+        let slugB = Slug("b")!
+        let slugC = Slug("c")!
+        let state = AppModel(
+            recent: [
+                EntryStub(
+                    slug: slugA,
+                    title: "A",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                ),
+                EntryStub(
+                    slug: slugB,
+                    title: "b",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                ),
+                EntryStub(
+                    slug: slugC,
+                    title: "C",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                )
+            ]
+        )
+        let update = AppModel.update(
+            state: state,
+            action: .deleteEntry(slugB),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.recent!.count,
+            2,
+            "Entry count correctly set"
+        )
+        XCTAssertEqual(
+            update.state.recent![0].id,
+            slugA,
+            "Slug A is still first"
+        )
+        XCTAssertEqual(
+            update.state.recent![1].id,
+            slugC,
+            "Slug C moved up because slug B was removed"
+        )
+    }
+
+    func testShowRenameSheet() throws {
+        let state = AppModel()
+        let slug = Slug("floop-the-pig")!
+        let update = AppModel.update(
+            state: state,
+            action: .showRenameSheet(slug),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isRenameSheetShowing,
+            true,
+            "Rename sheet is shown"
+        )
+        XCTAssertEqual(
+            update.state.slugToRename,
+            slug,
+            "slugToRename was set"
+        )
+    }
+
+    func testHideRenameSheet() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .hideRenameSheet,
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isRenameSheetShowing,
+            false,
+            "Rename sheet is hidden"
+        )
+        XCTAssertEqual(
+            update.state.slugToRename,
+            nil,
+            "slugToRename was set"
+        )
+    }
+    
+    func testRenameSlugField() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .setRenameSlugField("I Floop the Pig"),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.renameSlugField,
+            "i-floop-the-pig",
+            "The Pig has been flooped."
+        )
+    }
+    
+    func testSetSearch() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .setSearch("I Summon my Corn Demon"),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.searchText,
+            "I Summon my Corn Demon",
+            "Set search returns same string"
+        )
+    }
+    
+    func testHideSearch() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .hideSearch,
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isSearchShowing,
+            false,
+            "SearchShowing is false"
+        )
+        
+        XCTAssertEqual(
+            update.state.searchText,
+            "",
+            "Search Text Returns Blank"
+        )
+    }
+
+    func testSave() throws {
+        let state = AppModel(
+            slug: Slug("floop-the-pig")!,
+            editorSaveState: .modified
+        )
+        let update = AppModel.update(
+            state: state,
+            action: .save,
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.editorSaveState,
+            .saving,
+            "Sets editor save state to saving when not already saved"
+        )
+    }
+
+    func testSaveAlreadySaved() throws {
+        let state = AppModel(
+            slug: Slug("floop-the-pig")!,
+            editorSaveState: .saved
+        )
+        let update = AppModel.update(
+            state: state,
+            action: .save,
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.editorSaveState,
+            .saved,
+            "Leaves editor save state as saved if already saved"
         )
     }
 }
