7ff4e779afad38c6817120c10cbfdaad7077c427
Naginat4 <103612133+Naginat4@users.noreply.github.com>
2022-05-03 10:50:31 -0700

    Add tests for some update fns
    Add basic state tests for
    
    - entryCount
    - deleteEntry
    - showRenameSheet

--- xcode/Subconscious/SubconsciousTests/Tests_AppModelUpdate.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_AppModelUpdate.swift
@@ -94,4 +94,87 @@ class Tests_AppModelUpdate: XCTestCase {
             "Autofocus sets editor focus"
         )
     }
+
+    func testEntryCount() throws {
+        let state = AppModel()
+        let update = AppModel.update(
+            state: state,
+            action: .setEntryCount(10),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.entryCount,
+            10,
+            "Entry count correctly set"
+        )
+    }
+
+    func testDeleteEntry() throws {
+        let slugA = Slug("a")!
+        let slugB = Slug("b")!
+        let slugC = Slug("c")!
+        let state = AppModel(
+            recent: [
+                EntryStub(
+                    slug: slugA,
+                    title: "A",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                ),
+                EntryStub(
+                    slug: slugB,
+                    title: "b",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                ),
+                EntryStub(
+                    slug: slugC,
+                    title: "C",
+                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum orci quis lorem semper porta. Integer sem eros, ultricies et risus id, congue tristique libero.",
+                    modified: Date.now
+                )
+            ]
+        )
+        let update = AppModel.update(
+            state: state,
+            action: .deleteEntry(slugB),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.recent!.count,
+            2,
+            "Entry count correctly set"
+        )
+        XCTAssertEqual(
+            update.state.recent![0].id,
+            slugA,
+            "Slug A is still first"
+        )
+        XCTAssertEqual(
+            update.state.recent![1].id,
+            slugC,
+            "Slug C moved up because slug B was removed"
+        )
+    }
+
+    func testShowRenameSheet() throws {
+        let state = AppModel()
+        let slug = Slug("floop-the-pig")!
+        let update = AppModel.update(
+            state: state,
+            action: .showRenameSheet(slug),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isRenameSheetShowing,
+            true,
+            "Rename sheet is shown"
+        )
+        XCTAssertEqual(
+            update.state.slugToRename,
+            slug,
+            "slugToRename was set"
+        )
+    }
 }
