5fac7280b5440d4b89ed9f26d8c7a023d3f3c99d
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-05-31 13:44:33 -0700

    Add tests for SubtextFile content and title

--- xcode/Subconscious/SubconsciousTests/Tests_SubtextFile.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_SubtextFile.swift
@@ -9,6 +9,24 @@ import XCTest
 @testable import Subconscious
 
 class Tests_SubtextFile: XCTestCase {
+    func testContentParsing() throws {
+        let entry = SubtextFile(
+            slug: Slug("a-farm-picture")!,
+            content: """
+            title: Fire and Ice
+            author: Robert Frost
+
+            Some say the world will end in fire,
+            Some say in ice.
+            """
+        )
+        XCTAssertEqual(
+            entry.content,
+            "Some say the world will end in fire,\nSome say in ice.",
+            "Entry description produces correctly formatted document"
+        )
+    }
+
     func testDescription() throws {
         let entry = SubtextFile(
             slug: Slug("a-farm-picture")!,
@@ -24,6 +42,23 @@ class Tests_SubtextFile: XCTestCase {
             String(describing: entry),
             "Title: Fire and Ice\nAuthor: Robert Frost\n\nSome say the world will end in fire,\nSome say in ice.",
             "Entry description produces correctly formatted document"
+        )
+    }
+
+    func testTitle() throws {
+        let entry = SubtextFile(
+            slug: Slug("a-farm-picture")!,
+            content: """
+            title: Fire and Ice
+            author: Robert Frost
+
+            Some say the world will end in fire,
+            Some say in ice.
+            """
+        )
+        XCTAssertEqual(
+            entry.title,
+            "Fire and Ice"
         )
     }
 }
