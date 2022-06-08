62869d17b1377558e54f942b1f7a24bbed8d6432
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-06-08 14:15:57 -0500

    Make EntryLink fields let constants

--- xcode/Subconscious/Shared/Models/EntryStub.swift
+++ xcode/Subconscious/Shared/Models/EntryStub.swift
@@ -15,19 +15,9 @@ struct EntryStub:
     Identifiable,
     CustomDebugStringConvertible
 {
-    var link: EntryLink
-    var excerpt: String
-    var modified: Date
-
-    init(
-        link: EntryLink,
-        excerpt: String,
-        modified: Date
-    ) {
-        self.link = link
-        self.excerpt = excerpt
-        self.modified = modified
-    }
+    let link: EntryLink
+    let excerpt: String
+    let modified: Date
 
     var slug: Slug { link.slug }
     var title: String { link.linkableTitle }
