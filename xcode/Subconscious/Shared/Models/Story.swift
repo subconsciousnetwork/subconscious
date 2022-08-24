845b928edc410dcdc5b885e10c8568bac189dd2d
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-08-24 17:21:54 -0400

    Make Story Identifiable

--- xcode/Subconscious/Shared/Models/Story.swift
+++ xcode/Subconscious/Shared/Models/Story.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 /// Enum of possible story types for feed
-enum Story: Hashable {
+enum Story: Hashable, Identifiable {
     case prompt(StoryPrompt)
+
+    var id: String {
+        switch self {
+        case .prompt(let prompt):
+            return prompt.id
+        }
+    }
 }
