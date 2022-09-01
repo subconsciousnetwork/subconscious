060c6e057634201a575cf85126795fbf2f45283e
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-09-01 09:42:13 -0400

    Add elm-style MarkupTextView component

---
+++ xcode/Subconscious/Shared/Components/Common/MarkupTextViewRepresentable2.swift
@@ -0,0 +1,452 @@
+//
+//  MarkupTextViewRepresentable2.swift
+//  Subconscious (iOS)
+//
+//  Created by Gordon Brander on 7/6/21.
+//
+//  Note: we used to expose a way to set the `UITextView.font` property.
+//
+//  DO NOT SET `UITextView.font` property when using attributedText!
+//  It is meant for when you use `UITextView.text` without attributes.
+//  Setting it WHILE setting `attributedText` causes multiple issues:
+//
+//  - Styling properties will be overwritten by attributes given to
+//    `.font`. They seem to be applied after
+//    assigning `attributedText`.
+//  - You can create accidental feedback loops between the event
+//    delegate and the representable. This is because when you set
+//    attributedText on the UITextView, the attributed set on the
+//    property will no longer be equal to the attributedText before
+//    setting, since the font properties will be appended, modifying
+//    the attributes.
+//
+//  Lesson learned.
+//  2021-10-04 Gordon Brander
+//
+//  We now render text properties via an `NSTextStorageDelegate`.
+//  All styling is derived from markup. You should never have to set styling
+//  on the UITextView yourself when consuming this view.
+//  See https://github.com/gordonbrander/subconscious/pull/220/
+//  See https://github.com/gordonbrander/subconscious/issues/211
+//  2022-03-17 Gordon Brander
+import os
+import SwiftUI
+import ObservableStore
+
+enum FocusAction<Focus>: Hashable
+where Focus: Hashable
+{
+    /// Request a state-driven focus change
+    case requestFocus(Focus?)
+    case focusRequestScheduled
+    /// Notify of a focus change from the UI
+    case focusChange(Focus?)
+}
+
+struct FocusModel<Focus>: Hashable
+where Focus: Hashable
+{
+    typealias Model = Self
+    typealias Action = FocusAction<Focus>
+
+    var focusRequestScheduled = false
+    var focusRequest: Focus?
+    var focus: Focus?
+
+    static func update(
+        state: Model,
+        action: Action,
+        environment: Void
+    ) -> Update<Model, Action> {
+        switch action {
+        case .requestFocus(let focus):
+            var model = state
+            model.focusRequestScheduled = false
+            model.focusRequest = focus
+            return Update(state: model)
+        case .focusRequestScheduled:
+            var model = state
+            model.focusRequestScheduled = true
+            return Update(state: model)
+        /// UI-driven focus changes always win
+        case .focusChange(let focus):
+            var model = state
+            model.focusRequestScheduled = false
+            model.focusRequest = focus
+            model.focus = focus
+            return Update(state: model)
+        }
+    }
+}
+
+struct MarkupTextFocusCursor<Focus>: CursorProtocol
+where Focus: Hashable
+{
+    static func get(state: MarkupTextModel<Focus>) -> FocusModel<Focus> {
+        <#code#>
+    }
+    
+    static func set(state: MarkupTextModel<Focus>, inner: FocusModel<Focus>) -> MarkupTextModel<Focus> {
+        <#code#>
+    }
+    
+    static func tag(action: FocusAction<Focus>) -> MarkupTextAction<Focus> {
+        <#code#>
+    }
+    
+    typealias OuterState = MarkupTextModel<Focus>
+    typealias InnerState = FocusModel<Focus>
+    typealias OuterAction = MarkupTextAction<Focus>
+    typealias InnerAction = FocusAction<Focus>
+    
+    
+}
+
+enum MarkupTextAction<Focus>: Hashable
+where Focus: Hashable
+{
+    /// Request a state-driven focus change
+    case requestFocus(Focus?)
+    case focusRequestScheduled
+    /// Notify of a focus change from the UI
+    case notifyFocus(Focus?)
+    case setText(String)
+    case setSelection(NSRange)
+}
+
+struct MarkupTextModel<Focus>: Hashable
+where Focus: Hashable
+{
+    typealias Model = Self
+    typealias Action = MarkupTextAction<Focus>
+
+    var focusRequestScheduled = false
+    var focusRequest: Focus?
+    var focusState: Focus?
+    var text: String
+    var selection: NSRange
+
+    static func update(
+        state: Model,
+        action: Action,
+        environment: Void
+    ) -> Update<Model, Action> {
+        switch action {
+        case .requestFocus(let focus):
+            var model = state
+            model.focusRequestScheduled = false
+            model.focusRequest = focus
+            return Update(state: model)
+        case .focusRequestScheduled:
+            var model = state
+            model.focusRequestScheduled = true
+            return Update(state: model)
+        case .notifyFocus(let focus):
+            var model = state
+            model.focusRequestScheduled = false
+            model.focusState = focus
+            model.focusRequest = focus
+            return Update(state: model)
+        case .setText(let text):
+            var model = state
+            model.text = text
+            return Update(state: model)
+        case .setSelection(let selection):
+            var model = state
+            model.selection = selection
+            return Update(state: model)
+        }
+    }
+}
+
+/// A textview that grows to the height of its content
+struct MarkupTextViewRepresentable2<Focus>: UIViewRepresentable
+where Focus: Hashable
+{
+    //  MARK: UITextView subclass
+    class MarkupTextView: UITextView {
+        var fixedWidth: CGFloat = 0
+
+        override var intrinsicContentSize: CGSize {
+            sizeThatFits(
+                CGSize(
+                    width: fixedWidth,
+                    height: frame.height
+                )
+            )
+        }
+    }
+
+    //  MARK: Coordinator
+    class Coordinator: NSObject, UITextViewDelegate, NSTextStorageDelegate {
+        /// Is event happening during updateUIView?
+        /// Used to avoid setting properties in events during view updates, as
+        /// that would cause feedback cycles where an update triggers an event,
+        /// which triggers an update, which triggers an event, etc.
+        var isUIViewUpdating: Bool
+        // Dirty flag signaling if focus change has been requested already
+        var isAwaitingFocusChange: Bool
+        var representable: MarkupTextViewRepresentable2
+
+        init(
+            representable: MarkupTextViewRepresentable2
+        ) {
+            self.isUIViewUpdating = false
+            self.isAwaitingFocusChange = false
+            self.representable = representable
+        }
+
+        /// NSTextStorageDelegate method
+        /// Handle markup rendering, just before processEditing is fired.
+        /// It is important that we render markup in `willProcessEditing`
+        /// because it happens BEFORE font substitution. Rendering before font
+        /// substitution gives the OS a chance to replace fonts for things like
+        /// Emoji or Unicode characters when your font does not support them.
+        /// See:
+        /// https://github.com/gordonbrander/subconscious/wiki/nstextstorage-font-substitution-and-missing-text
+        ///
+        /// 2022-03-17 Gordon Brander
+        func textStorage(
+            _ textStorage: NSTextStorage,
+            willProcessEditing: NSTextStorage.EditActions,
+            range: NSRange,
+            changeInLength: Int
+        ) {
+            representable.logger?.debug(
+                "textStorage: render markup attributes"
+            )
+            textStorage.setAttributes(
+                [:],
+                range: NSRange(
+                    textStorage.string.startIndex...,
+                    in: textStorage.string
+                )
+            )
+            // Render markup on TextStorage (which is an NSMutableString)
+            // using closure set on view (representable)
+            self.representable.renderAttributesOf(textStorage)
+        }
+
+        /// Handle link taps
+        func textView(
+            _ textView: UITextView,
+            shouldInteractWith url: URL,
+            in characterRange: NSRange,
+            interaction: UITextItemInteraction
+        ) -> Bool {
+            representable.onLink(
+                url,
+                textView.attributedText,
+                characterRange,
+                interaction
+            )
+        }
+
+        /// Render user changes to textview
+        func textViewDidChange(_ view: UITextView) {
+            // Return early if view is updating.
+            guard !isUIViewUpdating else {
+                representable.logger?.debug(
+                    "textViewDidChange: View updating. Skipping."
+                )
+                return
+            }
+            self.representable.store.send(.setText(view.text))
+        }
+
+        /// Handle editing begin (focus)
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            // Mark focus clean
+            guard !isUIViewUpdating else {
+                representable.logger?.debug(
+                    "textViewDidBeginEditing: View updating. Skipping."
+                )
+                return
+            }
+            self.representable.store.send(.notifyFocus(representable.field))
+        }
+
+        /// Handle editing end (blur)
+        func textViewDidEndEditing(_ textView: UITextView) {
+            guard !isUIViewUpdating else {
+                representable.logger?.debug(
+                    "textViewDidEndEditing: View updating. Skipping."
+                )
+                return
+            }
+            self.representable.store.send(.notifyFocus(nil))
+        }
+
+        func textViewDidChangeSelection(_ textView: UITextView) {
+            // Return early if view is currently updating.
+            // We set selection during update when updating
+            // text, in order to retain selection.
+            // Do not set representable selection in this case. It would
+            // generate an update feedback loop, since the direction of
+            // mutation goes from representable to view during an update.
+            // 2021-10-06 Gordon Brander
+            guard !isUIViewUpdating else {
+                representable.logger?.debug(
+                    "textViewDidChangeSelection: View updating. Skipping."
+                )
+                return
+            }
+            representable.store.send(.setSelection(textView.selectedRange))
+        }
+    }
+
+    //  MARK: Properties
+    var store: ViewStore<MarkupTextModel<Focus>, MarkupTextAction<Focus>>
+    var field: Focus
+    /// Function to render NSAttributedString attributes from a markup string.
+    /// The renderer will use these attributes to style the string.
+    var renderAttributesOf: (NSMutableAttributedString) -> Void
+    var onLink: (
+        URL,
+        NSAttributedString,
+        NSRange,
+        UITextItemInteraction
+    ) -> Bool
+    var logger: Logger?
+    /// Frame needed to determine textview height.
+    /// Use `GeometryView` to find container width.
+    var frame: CGRect
+    var textColor: UIColor = UIColor(.primary)
+    var textContainerInset: UIEdgeInsets = .zero
+
+    //  MARK: makeUIView
+    func makeUIView(context: Context) -> MarkupTextView {
+        logger?.debug("makeUIView")
+        let view = MarkupTextView()
+
+        // Coordinator is both an UITextViewDelegate
+        // and an NSTextStorageDelegate.
+        // Set delegate on textview (coordinator)
+        view.delegate = context.coordinator
+        // Set delegate on textstorage (coordinator)
+        view.textStorage.delegate = context.coordinator
+
+        // Set inner padding
+        view.textContainerInset = self.textContainerInset
+        // Set width (needed to calculate height based on text length)
+        view.fixedWidth = self.frame.width
+        // Remove that extra bit of inner padding.
+        // Text in view should now be flush with view edge.
+        // This puts you in full control of view padding.
+        view.textContainer.lineFragmentPadding = 0
+        view.backgroundColor = .clear
+        view.textColor = textColor
+        view.isScrollEnabled = false
+        return view
+    }
+
+    //  MARK: updateUIView
+    /// Note that this function gets called every time the parent of
+    /// MarkupTextViewRepresentable has to recalculate its `body` property.
+    ///
+    /// You should think of this function as a hot loop.
+    ///
+    /// Avoid bashing properties that have side-effects. If you need to
+    /// set a property that has side-effects, check that it is actually
+    /// out of sync with the binding before setting, using an if-statement
+    /// or guard.
+    func updateUIView(_ view: MarkupTextView, context: Context) {
+        logger?.debug("updateUIView")
+        // Set updating flag on coordinator so that event callbacks
+        // can know if they are being called during an update.
+        context.coordinator.isUIViewUpdating = true
+        // Unset updating flag on coordator after this scope exits
+        defer {
+            context.coordinator.isUIViewUpdating = false
+        }
+
+        // Update text
+        if view.text != store.state.text {
+            logger?.debug("updateUIView: set text")
+            view.text = store.state.text
+        }
+
+        // Update width
+        if view.fixedWidth != self.frame.width {
+            logger?.debug("updateUIView: set width")
+            view.fixedWidth = self.frame.width
+        }
+
+        // Update view focus
+        self.updateUIViewFocus(view, context: context)
+
+        // Set selection
+        if store.state.selection != view.selectedRange {
+            logger?.debug("updateUIView: set selection")
+            view.selectedRange = self.store.state.selection
+        }
+
+        if self.textContainerInset != view.textContainerInset {
+            logger?.debug("updateUIView: set inset")
+            // Set inner padding
+            view.textContainerInset = self.textContainerInset
+        }
+    }
+
+    /// If representable focus state is out of sync
+    /// with `view.isFirstResponder`, then focus is dirty.
+    /// Schedule first responder change in response, and
+    /// flag that we are awaiting a focus change.
+    /// The flag prevents us from accidentally requesting focus change
+    /// more than once.
+    func updateUIViewFocus(_ view: MarkupTextView, context: Context) {
+        // Focus is clean, do nothing
+        guard store.state.focusRequest != store.state.focusState else {
+            return
+        }
+        guard !store.state.focusRequestScheduled else {
+            logger?.debug(
+                "updateUIViewFocus: Focus is dirty but focus change already scheduled. Skipping."
+            )
+            return
+        }
+        logger?.debug(
+            "updateUIViewFocus: scheduling first responder change"
+        )
+        store.send(.focusRequestScheduled)
+        // Call for first responder change needs to be async,
+        // or else we get an AttributeGraph cycle warning from SwiftUI.
+        // This is true for both becomeFirstResponder and resignFirstResponder.
+        // 2022-03-21 Gordon Brander
+        DispatchQueue.main.async {
+            // Check again in this tick to make sure we still need to
+            // change first responder state.
+            if store.state.focusRequest != store.state.focusState {
+                if store.state.focusRequest == field {
+                    logger?.debug(
+                        "async: call becomeFirstResponder"
+                    )
+                    view.becomeFirstResponder()
+                } else {
+                    logger?.debug(
+                        "async: call resignFirstResponder"
+                    )
+                    view.resignFirstResponder()
+                }
+            }
+        }
+        return
+    }
+
+    func makeCoordinator() -> Self.Coordinator {
+        Coordinator(
+            representable: self
+        )
+    }
+
+    func insets(_ inset: EdgeInsets) -> Self {
+        var view = self
+        view.textContainerInset = UIEdgeInsets(
+            top: inset.top,
+            left: inset.leading,
+            bottom: inset.bottom,
+            right: inset.trailing
+        )
+        return view
+    }
+}
