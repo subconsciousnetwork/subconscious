f3fa3be0a72c921cff6cd9591c4787d7dbe35528
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-09-09 16:56:04 -0400

    Fix remaining type errors

--- xcode/Subconscious/SubconsciousTests/Tests_Detail.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_Detail.swift
@@ -60,4 +60,96 @@ class Tests_Detail: XCTestCase {
             "Marks modified time"
         )
     }
+
+    func testSetEditorFocus() {
+        let store = Store(
+            update: DetailModel.update,
+            state: DetailModel(
+                slug: Slug("great-expectations")!,
+                saveState: .modified,
+                isLoading: false,
+                markupEditor: MarkupTextModel(
+                    text: "Mr. Pumblechook’s premises in the High Street of the market town, were of a peppercorny and farinaceous character."
+                )
+            ),
+            environment: environment
+        )
+
+        store.send(.requestEditorFocus(true))
+
+        let expectation = XCTestExpectation(
+            description: "focus set to nil"
+        )
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            XCTAssertEqual(
+                store.state.markupEditor.focusRequest,
+                true,
+                "Focus request was set"
+            )
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.2)
+    }
+
+
+    func testShowRenameSheet() throws {
+        let state = DetailModel()
+        let link = EntryLink(title: "Floop the Pig")!
+        let update = DetailModel.update(
+            state: state,
+            action: .showRenameSheet(link),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isRenameSheetShowing,
+            true,
+            "Rename sheet is shown"
+        )
+        XCTAssertEqual(
+            update.state.entryToRename,
+            link,
+            "slugToRename was set"
+        )
+    }
+
+    func testHideRenameSheet() throws {
+        let state = DetailModel()
+        let update = DetailModel.update(
+            state: state,
+            action: .hideRenameSheet,
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.isRenameSheetShowing,
+            false,
+            "Rename sheet is hidden"
+        )
+        XCTAssertEqual(
+            update.state.entryToRename,
+            nil,
+            "slugToRename was set"
+        )
+    }
+    
+    func testRenameField() throws {
+        let state = DetailModel(
+            entryToRename: EntryLink(
+                title: "Dawson spoke and there was music"
+            )!
+        )
+        let update = DetailModel.update(
+            state: state,
+            action: .setRenameField("Two pink faces turned in the flare of the tiny torch"),
+            environment: environment
+        )
+
+        XCTAssertEqual(
+            update.state.renameField,
+            "Two pink faces turned in the flare of the tiny torch",
+            "Rename field set to literal text of query"
+        )
+    }
+
 }
