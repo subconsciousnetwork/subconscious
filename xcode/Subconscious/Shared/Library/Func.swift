7d6e8d67cdf00164515698268b628eb01d36a4c3
Gordon Brander <gordonbrander@users.noreply.github.com>
2022-10-17 15:27:51 -0400

    Add pipe operator
    Following Javascript pipe operator proposal, this is a `|>` that pipes a
    single value through a series of one-arity functions. There are
    variations for normal, throwing, and optional functions.

--- xcode/Subconscious/Shared/Library/Func.swift
+++ xcode/Subconscious/Shared/Library/Func.swift
@@ -14,7 +14,7 @@ struct Func {
     ) -> U {
         through(value)
     }
-
+    
     /// Return the result of a closure immediately.
     /// This is useful for working around some of Swift's syntactical
     /// shortcomings. In particular, this lets us treat switch as an
@@ -22,4 +22,28 @@ struct Func {
     static func block<T>(_ closure: () -> T) -> T {
         closure()
     }
+}
+
+precedencegroup PipeOperator {
+    associativity: left
+}
+
+infix operator |>: PipeOperator
+
+/// Pipe a value through functions
+func |> <A, B>(lhs: A, rhs: (A) -> B) -> B {
+    rhs(lhs)
+}
+
+/// Pipe a value through a throwing function
+func |> <A, B>(lhs: A, rhs: (A) throws -> B) throws -> B {
+    try rhs(lhs)
+}
+
+/// Pipe an optional value through an optional mapping function
+func |> <A, B>(lhs: A?, rhs: (A) -> B?) -> B? {
+    guard let value = lhs else {
+        return nil
+    }
+    return rhs(value)
 }
