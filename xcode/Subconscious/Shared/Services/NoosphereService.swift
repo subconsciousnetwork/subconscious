98632f3faf6febd9780b9d57380d582b916636ff
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-01-11 14:29:39 -0500

    Create and configure a sphere

--- xcode/Subconscious/Shared/Services/NoosphereService.swift
+++ xcode/Subconscious/Shared/Services/NoosphereService.swift
@@ -4,63 +4,100 @@
 //
 //  Created by Gordon Brander on 1/10/23.
 //
+//  Noosphere lifecycle:
+//  1. Initialize namespace (once ever)
+//  2. Create key (once per key)
+//  3. Create sphere for key (once per key)
+//  4. Retreive sphere identity from sphere creation receipt, then store (once, upon sphere creation)
+//  5. Retrieve sphere mnemonic from sphere creation receipt. Display to user, then discard (once, upon sphere creation).
+//  6. Open sphere file system (on-demand)
 
 import Foundation
 import SwiftNoosphere
 
 public enum NoosphereServiceError: Error {
-    case unwrap(String)
+    case foreignError(String)
+}
+
+public struct SphereConfig {
+    var identity: String
+    var mnemonic: String
 }
 
 /// A Swift Wrapper for Noosphere.
 /// This class is designed to follow RAII pattern https://en.wikipedia.org/wiki/Resource_acquisition_is_initialization.
 /// Memory is automatically freed when the class is deallocated.
 public final class NoosphereService {
-    public let keyName: String
-    public let globalStoragePath: String
-    public let sphereStoragePath: String
-    public let sphereIdentity: String
-    public let sphereMnemonic: String
-    private let noosphere: OpaquePointer
-    private let sphereReceipt: OpaquePointer
-    private let sphereIdentityPointer: UnsafeMutablePointer<CChar>
-    private let sphereMnemonicPointer: UnsafeMutablePointer<CChar>
-
-    init(
-        keyName: String,
+    /// Create and configure a user and sphere
+    /// - Important: the identity should be persisted
+    /// - Important: the mnemonic should be displayed to user and then discarded.
+    /// - Returns: a SphereConfig containing the sphere identity and mnemonic
+    ///
+    /// What it does:
+    /// - Initializes a namespace
+    /// - Creates a key
+    /// - Creates a sphere for that key
+    public static func create(
+        ownerKeyName: String,
         globalStoragePath: String,
         sphereStoragePath: String
-    ) throws {
-        self.keyName = keyName
-        self.globalStoragePath = globalStoragePath
-        self.sphereStoragePath = sphereStoragePath
-        self.noosphere = ns_initialize(
-            self.globalStoragePath,
-            self.sphereStoragePath,
+    ) throws -> SphereConfig {
+        guard let noosphere = ns_initialize(
+            globalStoragePath,
+            sphereStoragePath,
             nil
-        )
+        ) else {
+            throw NoosphereServiceError.foreignError(
+                "Failed to get pointer for namespace"
+            )
+        }
+        defer {
+            ns_free(noosphere)
+        }
         
-        ns_key_create(noosphere, keyName)
-
-        self.sphereReceipt = ns_sphere_create(noosphere, self.keyName)
-
-        guard let sphereIdentityPointer = ns_sphere_receipt_identity(sphereReceipt) else {
-            throw NoosphereServiceError.unwrap("Failed to unwrap Sphere identity pointer")
+        ns_key_create(noosphere, ownerKeyName)
+        
+        guard let sphereReceipt = ns_sphere_create(
+            noosphere,
+            ownerKeyName
+        ) else {
+            throw NoosphereServiceError.foreignError(
+                "Failed to get pointer for sphere receipt"
+            )
         }
-        self.sphereIdentityPointer = sphereIdentityPointer
+        defer {
+            ns_sphere_receipt_free(sphereReceipt)
 
-        guard let sphereMnemonicPointer = ns_sphere_receipt_mnemonic(sphereReceipt) else {
-            throw NoosphereServiceError.unwrap("Failed to unwrap Sphere mnemonic pointer")
         }
-        self.sphereMnemonicPointer = sphereMnemonicPointer
-        self.sphereIdentity = String.init(cString: sphereIdentityPointer)
-        self.sphereMnemonic = String.init(cString: sphereMnemonicPointer)
-    }
-
-    deinit {
-        ns_string_free(self.sphereIdentityPointer)
-        ns_string_free(self.sphereMnemonicPointer)
-        ns_sphere_receipt_free(self.sphereReceipt)
-        ns_free(self.noosphere)
+        
+        guard let sphereIdentityPointer = ns_sphere_receipt_identity(
+            sphereReceipt
+        ) else {
+            throw NoosphereServiceError.foreignError(
+                "Failed to get pointer for identity"
+            )
+        }
+        defer {
+            ns_string_free(sphereIdentityPointer)
+        }
+        
+        guard let sphereMnemonicPointer = ns_sphere_receipt_mnemonic(
+            sphereReceipt
+        ) else {
+            throw NoosphereServiceError.foreignError(
+                "Failed to get pointer for mnemonic"
+            )
+        }
+        defer {
+            ns_string_free(sphereMnemonicPointer)
+        }
+        
+        let sphereIdentity = String.init(cString: sphereIdentityPointer)
+        let sphereMnemonic = String.init(cString: sphereMnemonicPointer)
+        
+        return SphereConfig(
+            identity: sphereIdentity,
+            mnemonic: sphereMnemonic
+        )
     }
 }
