59112366650d28f6249b9d86784251bed1eab21c
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-01-11 20:02:19 -0500

    Introduce Omnibox view

---
+++ xcode/Subconscious/Shared/Components/OmniboxView.swift
@@ -0,0 +1,48 @@
+//
+//  OmniboxView.swift
+//  Subconscious
+//
+//  Created by Gordon Brander on 1/11/23.
+//
+
+import SwiftUI
+
+struct OmniboxView: View {
+    var icon: Image
+    var title: String
+
+    var body: some View {
+        HStack(spacing: 8) {
+            icon
+                .resizable()
+                .frame(width: 17, height: 17)
+                .foregroundColor(.accentColor)
+            HStack(spacing: 0) {
+                Spacer()
+                Text(verbatim: title)
+                    .foregroundColor(.accentColor)
+                Spacer()
+            }
+        }
+        .padding(.leading, 8)
+        .padding(.trailing, 12)
+        .frame(height: 36)
+        .background(Color.secondaryBackground)
+        .cornerRadius(12)
+    }
+}
+
+struct OmniboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            OmniboxView(
+                icon: Image(systemName: "network"),
+                title: "Permissionless creation is where value comes from"
+            )
+            OmniboxView(
+                icon: Image(systemName: "network"),
+                title: "X"
+            )
+        }
+    }
+}
