79b81a216252e6849c716ea15c28744ccd2f91e6
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-02-06 08:36:47 -0500

    Remove tests for array+get
    We no longer use this method, and have removed it.

--- xcode/Subconscious/SubconsciousTests/Tests_CollectionUtilities.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_CollectionUtilities.swift
@@ -9,37 +9,6 @@ import XCTest
 @testable import Subconscious
 
 class Tests_CollectionUtilities: XCTestCase {
-
-    func testCollectionGetValidBounds() throws {
-        let collection = [0, 1, 2, 3]
-        let value = collection.get(1)
-        XCTAssertEqual(
-            value,
-            1,
-            "Collection.get retreives value at index within valid bounds"
-        )
-    }
-
-    func testCollectionGetInValidBounds() throws {
-        let collection = [0, 1, 2, 3]
-        let value = collection.get(1000)
-        XCTAssertEqual(
-            value,
-            nil,
-            "Collection.get returns nil for index outside of valid bounds"
-        )
-    }
-
-    func testCollectionEmpty() throws {
-        let collection: [Int] = []
-        let value = collection.get(0)
-        XCTAssertEqual(
-            value,
-            nil,
-            "Collection.get returns nil for index outside of valid bounds"
-        )
-    }
-
     func testUniquing() throws {
         let array = [1, 2, 1, 2, 3, 3, 1, 4]
         let unique = array.uniquing(with: { i in i })
