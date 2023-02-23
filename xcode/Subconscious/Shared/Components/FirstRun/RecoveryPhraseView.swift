88a00b9b30b43a894f115be0c2ab9c96342f9737
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-02-23 09:10:44 -0500

    Add copy/copied states to recovery phrase view (#405)
    We make RecoveryPhraseView a component that knows how to manage its own state and side-effects, when stitched into a larger component.
    
    Fixes #400.
    
    Introduces:
    
    - RecoveryPhraseModel
    - RecoveryPhraseAction
    - AppRecoveryPhraseCursor to map from app domain to recovery phrase domain

--- xcode/Subconscious/Shared/Components/FirstRun/RecoveryPhraseView.swift
+++ xcode/Subconscious/Shared/Components/FirstRun/RecoveryPhraseView.swift
@@ -6,14 +6,17 @@
 //
 
 import SwiftUI
+import ObservableStore
+import os
 
 struct RecoveryPhraseView: View {
-    var text: String
+    var state: RecoveryPhraseModel
+    var send: (RecoveryPhraseAction) -> Void
 
     var body: some View {
         VStack(spacing: 0) {
             HStack {
-                Text(text)
+                Text(state.phrase)
                     .monospaced()
                     .textSelection(.enabled)
                 Spacer()
@@ -21,12 +24,30 @@ struct RecoveryPhraseView: View {
             .padding()
             Button(
                 action: {
-                    UIPasteboard.general.string = text
+                    send(.copy)
                 },
                 label: {
-                    HStack {
-                        Image(systemName: "doc.on.doc")
-                        Text("Copy to clipboard")
+                    if !state.didCopy {
+                        HStack {
+                            Image(systemName: "doc.on.doc")
+                            Text("Copy to clipboard")
+                        }
+                        .transition(
+                            .asymmetric(
+                                insertion: .identity,
+                                removal: .move(
+                                    edge: .top
+                                ).combined(
+                                    with: .opacity
+                                )
+                            )
+                        )
+                    } else {
+                        HStack {
+                            Image(systemName: "checkmark.circle")
+                            Text("Copied!")
+                        }
+                        .transition(.opacity)
                     }
                 }
             )
@@ -42,10 +63,73 @@ struct RecoveryPhraseView: View {
     }
 }
 
+enum RecoveryPhraseAction: Hashable {
+    case setPhrase(_ phrase: String)
+    case copy
+}
+
+struct RecoveryPhraseModel: ModelProtocol {
+    var phrase: String = ""
+    var didCopy: Bool = false
+
+    static let logger = Logger(
+        subsystem: Config.default.rdns,
+        category: "RecoveryPhrase"
+    )
+
+    static func update(
+        state: RecoveryPhraseModel,
+        action: RecoveryPhraseAction,
+        environment: RecoveryPhraseEnvironment
+    ) -> Update<RecoveryPhraseModel> {
+        switch action {
+        case .setPhrase(let phrase):
+            var model = state
+            model.phrase = phrase
+            return Update(state: model)
+        case .copy:
+            // Copy to clipboard
+            environment.pasteboard.string = state.phrase
+            logger.log("Copied recovery phrase to clipboard")
+            var model = state
+            model.didCopy = true
+            return Update(state: model)
+                .animation(.default)
+        }
+    }
+}
+
+/// Exposes a minimal API surface to the pasteboard.
+/// Just the parts we use here.
+protocol RecoveryPhrasePasteboardProtocol {
+    var string: String? { get nonmutating set }
+}
+
+extension UIPasteboard: RecoveryPhrasePasteboardProtocol {}
+
+struct RecoveryPhraseEnvironment {
+    // Pasteboard is defined as a protocol. This allows us to mock for testing.
+    var pasteboard: RecoveryPhrasePasteboardProtocol = UIPasteboard.general
+}
+
 struct RecoveryPhraseView_Previews: PreviewProvider {
-    static var previews: some View {
-        RecoveryPhraseView(
-            text: "foo bar baz bing bong boo biz boz bonk bink boop bop beep bleep bloop blorp blonk blink blip blop boom"
+    struct TestView: View {
+        @StateObject private var store = Store(
+            state: RecoveryPhraseModel(
+                phrase: "foo bar baz bing bong boo biz boz bonk bink boop bop beep bleep bloop blorp blonk blink blip blop boom"
+            ),
+            environment: RecoveryPhraseEnvironment()
         )
+
+        var body: some View {
+            RecoveryPhraseView(
+                state: store.state,
+                send: store.send
+            )
+        }
+    }
+
+    static var previews: some View {
+        TestView()
     }
 }
---
+++ xcode/Subconscious/SubconsciousTests/Tests_RecoveryPhrase.swift
@@ -0,0 +1,61 @@
+//
+//  Tests_RecoveryPhrase.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 2/22/23.
+//
+
+import XCTest
+@testable import Subconscious
+
+final class Tests_RecoveryPhrase: XCTestCase {
+    final class MockPasteboard: RecoveryPhrasePasteboardProtocol {
+        var sets: Int = 0
+        var _string: String? = nil
+
+        var string: String? {
+            get { _string }
+            set {
+                self._string = newValue
+                self.sets = self.sets + 1
+            }
+        }
+    }
+
+    func testSetPhrase() throws {
+        let environment = RecoveryPhraseEnvironment(
+            pasteboard: MockPasteboard()
+        )
+
+        let state = RecoveryPhraseModel()
+        XCTAssertEqual(state.phrase, "")
+
+        let update = RecoveryPhraseModel.update(
+            state: state,
+            action: .setPhrase("foo bar baz"),
+            environment: environment
+        )
+        XCTAssertEqual(update.state.phrase, "foo bar baz", "Set phrase")
+    }
+    
+    func testCopy() throws {
+        let pasteboard = MockPasteboard()
+        let environment = RecoveryPhraseEnvironment(
+            pasteboard: pasteboard
+        )
+
+        let state = RecoveryPhraseModel(
+            phrase: "foo bar baz"
+        )
+        XCTAssertEqual(state.didCopy, false, "didCopy defaults to false")
+
+        let update = RecoveryPhraseModel.update(
+            state: state,
+            action: .copy,
+            environment: environment
+        )
+        XCTAssertEqual(update.state.didCopy, true, "Updated didCopy")
+        XCTAssertEqual(pasteboard.sets, 1, "Copied")
+        XCTAssertNotNil(update.transaction, "Has animation")
+    }
+}
