aad77934584cece7219965dd4a168450325e5d5f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-02-27 11:19:41 +1000

    Stub AddressBookView
    Lists petname + DID (with pfp, though unclear how we'll get access to that at this stage)

---
+++ xcode/Subconscious/Shared/Components/AddressBook/AddressBookEntryView.swift
@@ -0,0 +1,39 @@
+//
+//  AddressBookEntryView.swift
+//  Subconscious (iOS)
+//
+//  Created by Ben Follington on 2/27/23.
+//
+
+import SwiftUI
+
+/// Full-sized user card, intended for listing users
+struct AddressBookEntryView: View {
+    var pfp: Image
+    var petname: String
+    var did: String
+    
+    var body: some View {
+        HStack(spacing: AppTheme.unit3) {
+            ProfilePic(image: pfp)
+            VStack(alignment: .leading) {
+                Text(verbatim: petname)
+                    .foregroundColor(.buttonText)
+                    .fontWeight(.semibold)
+                Text(verbatim: did)
+                    .foregroundColor(.secondary)
+                    .font(.caption)
+            }
+        }
+    }
+}
+
+struct AddressBookEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressBookEntryView(
+            pfp: Image("pfp-dog"),
+            petname: "@name",
+            did: "did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7"
+        )
+    }
+}
---
+++ xcode/Subconscious/Shared/Components/AddressBook/AddressBookView.swift
@@ -0,0 +1,73 @@
+//
+//  AddressBookView.swift
+//  Subconscious (iOS)
+//
+//  Created by Ben Follington on 2/27/23.
+//
+
+import SwiftUI
+import ObservableStore
+
+struct AddressBookEntry {
+    var pfp: Image
+    var petname: String
+    var did: String
+}
+
+struct AddressBookView: View {
+    @ObservedObject var app: Store<AppModel>
+    var unknown = "Unknown"
+    
+    @State var following: [AddressBookEntry] = [
+        AddressBookEntry(pfp: Image("pfp-dog"), petname: "ben", did: "did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7"),
+        AddressBookEntry(pfp: Image("sub_logo_light"), petname: "bob", did: "did:key:z6MkmBJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")
+    ]
+    
+    func delete(at offsets: IndexSet) {
+        following.remove(atOffsets: offsets)
+    }
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                if (following.count == 0) {
+                    Text("No friends?")
+                } else {
+                    List {
+                        ForEach(following, id: \.did) { user in
+                            AddressBookEntryView(pfp: user.pfp, petname: user.petname,
+                                                 did: user.did)
+                        }
+                        .onDelete(perform: delete)
+                    }
+                }
+                
+            }
+            .navigationTitle("Address Book")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Close") {
+                        app.send(.presentSettingsSheet(false))
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add Friend") {
+                        app.send(.presentSettingsSheet(false))
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct AddressBook_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressBookView(
+            app: Store(
+                state: AppModel(),
+                environment: AppEnvironment()
+            )
+        )
+    }
+}
