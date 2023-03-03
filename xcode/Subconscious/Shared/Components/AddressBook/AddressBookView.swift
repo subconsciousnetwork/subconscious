a912dac1c8f32c8587b902269c4a808c21c6defe
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-03-03 16:50:20 +1000

    Implement AddressBookModel

--- xcode/Subconscious/Shared/Components/AddressBook/AddressBookView.swift
+++ xcode/Subconscious/Shared/Components/AddressBook/AddressBookView.swift
@@ -5,62 +5,85 @@
 //  Created by Ben Follington on 2/27/23.
 //
 
+import os
 import SwiftUI
 import ObservableStore
 
-struct AddressBookEntry {
+struct AddressBookEntry: Equatable {
     var pfp: Image
-    var petname: String
+    var petname: Petname
     var did: Did
 }
 
+struct AddressBookEnvironment { }
+
+enum AddressBookAction: Hashable {
+    case addFriend(did: Did, petname: Petname)
+    case removeFriend(did: Did)
+}
+
+struct AddressBookModel: ModelProtocol {
+    var friends: [AddressBookEntry] = []
+
+    static let logger = Logger(
+        subsystem: Config.default.rdns,
+        category: "AddressBookModel"
+    )
+
+    static func update(
+        state: AddressBookModel,
+        action: AddressBookAction,
+        environment: AddressBookEnvironment
+    ) -> Update<AddressBookModel> {
+        switch action {
+        case .addFriend(did: let did, petname: let petname):
+            let entry = AddressBookEntry(pfp: Image("pfp-dog"), petname: petname, did: did)
+            
+            var model = state
+            model.friends.append(entry)
+            return Update(state: model)
+        case .removeFriend(did: let did):
+            var model = state
+            
+            model.friends.removeAll { entry in
+                entry.did == did
+            }
+            return Update(state: model)
+        }
+    }
+}
+
 struct AddressBookView: View {
-    @ObservedObject var app: Store<AppModel>
-    var unknown = "Unknown"
+    var state: AddressBookModel
+    var send: (AddressBookAction) -> Void
     
-    @State var searchText = ""
-    
-    // Temp state, will be moved to slice of AppStore later
-    @State var didCopy = false
-    
-    // Temp state, will be moved to slice of AppStore later
-    @State var following: [AddressBookEntry] = [
-        AddressBookEntry(pfp: Image("pfp-dog"), petname: "ben", did: Did(  "did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!),
-        AddressBookEntry(pfp: Image("sub_logo_light"), petname: "bob", did: Did("did:key:z6MkmBJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!),
-        AddressBookEntry(pfp: Image("sub_logo_dark"), petname: "alice", did: Did("did:key:z6MjmBJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!)
-    ]
-    
+    // TODO: how can I actually get this information?
     var myDid = Did("did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!
     
     func delete(at offsets: IndexSet) {
-        following.remove(atOffsets: offsets)
-    }
-    
-    func filteredFollowing() -> [AddressBookEntry] {
-        return following.filter { user in
-            if (searchText.count > 0) {
-                return user.petname.lowercased().range(of: searchText.lowercased()) != nil
-            } else {
-                return true
-            }
+        guard let idx = offsets.first else {
+            return
+        }
+        
+        if let f = state.friends.get(idx) {
+            send(.removeFriend(did: f.did))
         }
     }
-
+    
     var body: some View {
         NavigationStack {
             Form {
-                if (following.count == 0) {
+                if (state.friends.count == 0) {
                     Text("No friends?")
                 } else {
                     Section(header: Text("Friends")) {
                         List {
-                            ForEach(filteredFollowing(), id: \.did) { user in
+                            ForEach(state.friends, id: \.did) { user in
                                 AddressBookEntryView(pfp: user.pfp, petname: user.petname,
                                                      did: user.did)
                             }
                             .onDelete(perform: delete)
                         }
-                        .searchable(text: $searchText)
                     }
                 }
                 Section(header: Text("My DID")) {
@@ -72,13 +95,13 @@ struct AddressBookView: View {
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button("Close") {
-                        app.send(.presentSettingsSheet(false))
+                        AddressBookModel.logger.debug("Close Address Book")
                     }
                 }
                 ToolbarItem(placement: .confirmationAction) {
                     NavigationLink(
                         destination: {
-                            AddFriendView(app: app)
+                            AddFriendView(state: state, send: send)
                         },
                         label: {
                     Text("Add Friend")
@@ -92,12 +115,27 @@ struct AddressBookView: View {
 }
 
 struct AddressBook_Previews: PreviewProvider {
-    static var previews: some View {
-        AddressBookView(
-            app: Store(
-                state: AppModel(),
-                environment: AppEnvironment()
-            )
+    struct TestView: View {
+        @StateObject private var store = Store(
+            state: AddressBookModel(
+                friends: [
+                    AddressBookEntry(pfp: Image("pfp-dog"), petname: Petname("ben")!, did: Did(  "did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!),
+                    AddressBookEntry(pfp: Image("sub_logo_light"), petname: Petname("bob")!, did: Did("did:key:z6MkmBJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!),
+                    AddressBookEntry(pfp: Image("sub_logo_dark"), petname: Petname("alice")!, did: Did("did:key:z6MjmBJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!)
+                ]
+            ),
+            environment: AddressBookEnvironment()
         )
+
+        var body: some View {
+            AddressBookView(
+                state: store.state,
+                send: store.send
+            )
+        }
+    }
+
+    static var previews: some View {
+        TestView()
     }
 }
