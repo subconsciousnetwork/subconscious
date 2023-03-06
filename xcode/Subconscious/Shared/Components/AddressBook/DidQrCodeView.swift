0c99479630fbb14f55d4cb1a67b4099f175f9c17
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-03-06 13:43:33 +1000

    Colorize generated QR code using accent color

--- xcode/Subconscious/Shared/Components/AddressBook/DidQrCodeView.swift
+++ xcode/Subconscious/Shared/Components/AddressBook/DidQrCodeView.swift
@@ -14,24 +14,48 @@ struct DidQrCodeView: View {
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
+    let colorInvertFilter = CIFilter.colorInvert()
+    let maskToAlphaFilter = CIFilter.maskToAlpha()
+    let colorReplaceFilter = CIFilter.spotColor()
     
-    // Probably best to extract this logic somewhere common
-    func generateQRCode(from string: String) -> UIImage {
+    func generateQRCode(from string: String) -> UIImage? {
         filter.message = Data(string.utf8)
-
-        if let outputImage = filter.outputImage {
-            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
-            }
-        }
         
-        // Appears we can style / customise this quite a lot, but might require digging into CoreImage internals. This libray might be useful: https://github.com/dagronf/QRCode
-
-        return UIImage(systemName: "xmark.circle") ?? UIImage()
+        guard let img = filter.outputImage else { return nil }
+        colorInvertFilter.inputImage = img
+        
+        // Black on White -> White on Black
+        guard let img = colorInvertFilter.outputImage else { return nil}
+        maskToAlphaFilter.inputImage = img
+        
+        // Mask Black to transparent
+        guard let img = maskToAlphaFilter.outputImage else { return nil }
+        colorReplaceFilter.inputImage = img
+        colorReplaceFilter.centerColor1 = .white
+        colorReplaceFilter.closeness1 = 1
+        
+        // This is the only way I managed to produce a CIColor from .accentColor
+        // https://developer.apple.com/forums/thread/687764
+        // https://stackoverflow.com/questions/56586055/how-to-get-rgb-components-from-color-in-swiftui
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 0.0
+        UIColor(Color.accentColor).getRed(&r, green: &g, blue: &b, alpha: &a)
+        colorReplaceFilter.replacementColor1 = CIColor(red: r, green: g, blue: b, alpha: a)
+        
+        // Replace White with accent color
+        guard let img = colorReplaceFilter.outputImage else { return nil }
+        guard let cgImg = context.createCGImage(img, from: img.extent) else { return nil }
+        
+        return UIImage(cgImage: cgImg)
     }
     
+    
     var body: some View {
-        Image(uiImage: generateQRCode(from: "\(did)"))
+        let img = generateQRCode(from: "\(did)") ?? UIImage(systemName: "xmark.circle") ?? UIImage()
+        
+        Image(uiImage: img)
             .resizable()
             .interpolation(.none)
             .scaledToFit()
