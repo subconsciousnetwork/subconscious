95494a4463e5c2e650ab99ad73a78e33a26fa013
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-03-07 15:19:47 +1000

    Introduce useful NSRange utils

--- xcode/Subconscious/Shared/Library/NSRangeUtilities.swift
+++ xcode/Subconscious/Shared/Library/NSRangeUtilities.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension NSRange {
     /// Determine if an NSRange is a valid range for a given string.
@@ -13,4 +14,27 @@ extension NSRange {
         let range = Range(self, in: string)
         return range != nil
     }
+}
+
+
+// https://gist.github.com/krzyzanowskim/057676af670f06fa6061473ac28a6c58
+extension NSRange {
+
+    static let notFound = NSRange(location: NSNotFound, length: 0)
+
+    var isEmpty: Bool {
+        length == 0
+    }
+
+    init(_ textRange: NSTextRange, in textContentManager: NSTextContentManager) {
+        let offset = textContentManager.offset(from: textContentManager.documentRange.location, to: textRange.location)
+        let length = textContentManager.offset(from: textRange.location, to: textRange.endLocation)
+        self.init(location: offset, length: length)
+    }
+
+    init(_ textLocation: NSTextLocation, in textContentManager: NSTextContentManager) {
+        let offset = textContentManager.offset(from: textContentManager.documentRange.location, to: textLocation)
+        self.init(location: offset, length: 0)
+    }
+
 }
