2f2e5038efd9168d2578aa4c5c25e80339f0e1fa
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-03-14 13:38:04 -0400

    Add Memo.draft

--- xcode/Subconscious/SubconsciousTests/Tests_Memo.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_Memo.swift
@@ -20,7 +20,7 @@ final class Tests_Memo: XCTestCase {
             ],
             body: "Example A"
         )
-
+        
         let b = Memo(
             contentType: ContentType.subtext.rawValue,
             created: Date.now,
@@ -32,21 +32,43 @@ final class Tests_Memo: XCTestCase {
             ],
             body: "Example B"
         )
-
+        
         let c = a.merge(b)
-
+        
         // Test that A's headers win
         XCTAssertEqual(c.fileExtension, a.fileExtension, "Keeps subject file extension")
-
+        
         // Test that additional headers are merged
         XCTAssertEqual(c.additionalHeaders.count, 2, "Merges headers")
         let foo = c.additionalHeaders.get(first: "Foo")
         XCTAssertEqual(foo, "Foo", "Merges headers")
-
+        
         XCTAssertEqual(
             c.body,
             "Example A\n\nExample B",
             "Concatenates bodies"
         )
+    }
+    
+    func testMemoDraftDefaults() throws {
+        let draft = Memo.draft(body: "Foo")
+        XCTAssertEqual(draft.body, "Foo")
+        XCTAssertEqual(draft.contentType, ContentType.subtext.rawValue)
+        XCTAssertEqual(draft.modified.ISO8601Format(), Date.now.ISO8601Format())
+        XCTAssertEqual(draft.created.ISO8601Format(), Date.now.ISO8601Format())
+    }
+    
+    func testMemoDraftCustom() throws {
+        let date = Date.distantPast
+        let draft = Memo.draft(
+            contentType: .text,
+            created: date,
+            modified: date,
+            body: "Foo"
+        )
+        XCTAssertEqual(draft.body, "Foo")
+        XCTAssertEqual(draft.contentType, ContentType.text.rawValue)
+        XCTAssertEqual(draft.modified, date)
+        XCTAssertEqual(draft.created, date)
     }
 }
