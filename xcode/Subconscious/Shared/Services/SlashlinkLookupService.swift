a9f2d794c5559f055a24384f207c994e4f431379
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-03-23 10:56:21 +1000

    Introduce simple in-memory cache for Slashlink preview

---
+++ xcode/Subconscious/Shared/Services/SlashlinkLookupService.swift
@@ -0,0 +1,51 @@
+//
+//  SlashlinkLookupService.swift
+//  Subconscious
+//
+//  Created by Ben Follington on 23/3/2023.
+//
+
+import Foundation
+import Combine
+
+class SlashlinkLookupService {
+    var database: DatabaseService
+    private var cache: Dictionary<Slashlink, EntryStub>
+    
+    init(database: DatabaseService) {
+        self.database = database
+        self.cache = Dictionary()
+    }
+    
+    func listEntriesForSlashlinks(slashlinks: [Slashlink]) throws -> [EntryStub] {
+        // Check for cached data
+        let cacheHits =
+            slashlinks.map { s in
+                cache[s]
+            }
+            .compactMap { value in value }
+        
+        // Do we have it all? Early return
+        if cacheHits.count == slashlinks.count {
+            return cacheHits
+        }
+        
+        // Otherwise, only look up the cache misses
+        let remainingLinks = slashlinks.filter { s in cache[s] == nil }
+        let result = try self.database.listEntriesForSlashlinks(slashlinks: remainingLinks)
+        
+        // Cache for the future
+        for link in result {
+            cache[link.address.toSlashlink()] = link
+        }
+        
+        // Combine the two sets of results
+        return result + cacheHits
+    }
+    
+    func listEntriesForSlashlinksAsync(slashlinks: [Slashlink]) -> AnyPublisher<[EntryStub], Error> {
+        CombineUtilities.async(qos: .default) {
+            return try self.listEntriesForSlashlinks(slashlinks: slashlinks)
+        }
+    }
+}
