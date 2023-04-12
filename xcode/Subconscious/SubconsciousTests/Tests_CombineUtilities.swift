35c86e136d015dd3aad9c2ef99443a87b4da9d5f
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-04-12 16:27:15 -0400

    Add Tests_CombineUtilities

---
+++ xcode/Subconscious/SubconsciousTests/Tests_CombineUtilities.swift
@@ -0,0 +1,74 @@
+//
+//  Tests_CombineUtilities.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 4/12/23.
+//
+
+import XCTest
+import Combine
+@testable import Subconscious
+
+final class Tests_CombineUtilities: XCTestCase {
+    enum TestError: Error {
+        case code(Int)
+    }
+
+    var cancellables: Set<AnyCancellable> = Set()
+    
+    override func setUp() {
+        cancellables = Set()
+    }
+    
+    func testDispatchQueueFuture() throws {
+        let future = DispatchQueue.global().future {
+            XCTAssertFalse(Thread.isMainThread, "Run in separate thread")
+            Thread.sleep(forTimeInterval: 0.2)
+            return 10
+        }
+        
+        let expectation = XCTestExpectation(
+            description: "Future succeeds"
+        )
+        
+        future.sink(
+            receiveCompletion: { completion in
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                XCTAssertEqual(value, 10)
+            }
+        )
+        .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 0.3)
+    }
+    
+    func testDispatchQueueError() throws {
+        let future: Future<Int, Error> = DispatchQueue.global().future {
+            throw TestError.code(10)
+        }
+
+        let expectation = XCTestExpectation(
+            description: "Future succeeds"
+        )
+
+        future.sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case let .failure(TestError.code(value)):
+                    XCTAssertEqual(value, 10)
+                    expectation.fulfill()
+                default:
+                    XCTFail("Incorrect completion: \(completion)")
+                }
+                return
+            },
+            receiveValue: { value in
+            }
+        )
+        .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 0.2)
+    }
+}
