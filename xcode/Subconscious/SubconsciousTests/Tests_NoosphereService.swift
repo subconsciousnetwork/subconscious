87bc669e66cbcb088699f44e9f910b796ad39c1c
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-04-14 17:26:37 -0400

    Update more tests

--- xcode/Subconscious/SubconsciousTests/Tests_NoosphereService.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_NoosphereService.swift
@@ -22,49 +22,58 @@ final class Tests_NoosphereService: XCTestCase {
         return url
     }
     
-    func testSphereTraversal() throws {
+    func testSphereTraversal() async throws {
         throw XCTSkip("Sphere Traversal relies on running a gateway, this test will not pass currently.")
         
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
-        let noosphere = data.noosphere
+        let globalStorageURL = tmp.appending(path: "noosphere")
+        let sphereStorageURL = tmp.appending(path: "sphere")
         
-        let bobReceipt = try noosphere.createSphere(ownerKeyName: "bob")
-        let aliceReceipt = try noosphere.createSphere(ownerKeyName: "alice")
+        let noosphere = NoosphereService(
+            globalStorageURL: globalStorageURL,
+            sphereStorageURL: sphereStorageURL,
+            gatewayURL: URL(string: "http://unavailable-gateway.fakewebsite")
+        )
+        
+        let receipt = try await noosphere.createSphere(ownerKeyName: "bob")
+        await noosphere.resetSphere(receipt.identity)
+
+        let bobReceipt = try await noosphere.createSphere(ownerKeyName: "bob")
+        let aliceReceipt = try await noosphere.createSphere(ownerKeyName: "alice")
         
         let bob = Petname("bob")!
         let alice = Petname("alice")!
 
         // Put bob in alice's address book
-        noosphere.resetSphere(aliceReceipt.identity)
-        try noosphere.setPetname(did: bobReceipt.identity, petname: bob)
-        try noosphere.save()
+        await noosphere.resetSphere(aliceReceipt.identity)
+        try await noosphere.setPetname(did: bobReceipt.identity, petname: bob)
+        try await noosphere.save()
         
-        let bobDid = try noosphere.getPetname(petname: bob)
+        let bobDid = try await noosphere.getPetname(petname: bob)
         XCTAssertEqual(bobDid, bobReceipt.identity)
         
         // Put alice in bob's address book & set bob as default sphere
-        noosphere.resetSphere(bobReceipt.identity)
-        try noosphere.setPetname(did: aliceReceipt.identity, petname: alice)
-        try noosphere.save()
+        await noosphere.resetSphere(bobReceipt.identity)
+        try await noosphere.setPetname(did: aliceReceipt.identity, petname: alice)
+        try await noosphere.save()
         
-        let aliceDid = try noosphere.getPetname(petname: alice)
+        let aliceDid = try await noosphere.getPetname(petname: alice)
         XCTAssertEqual(aliceDid, aliceReceipt.identity)
         
-        
-        _ = try noosphere.sync()
+        _ = try await noosphere.sync()
         // This should loop back around to bob
-        let destinationSphere = try noosphere
+        let destinationSphere = try await noosphere
             .traverse(petname: alice)
             .traverse(petname: bob)
 
         // Clear out Noosphere and Sphere instance
-        noosphere.reset()
+        await noosphere.reset()
         
-        XCTAssertEqual(destinationSphere.identity, bobReceipt.identity)
+        let sphereIdentity = try await destinationSphere.identity()
+        XCTAssertEqual(sphereIdentity, bobReceipt.identity)
     }
     
-    func testNoosphereReset() throws {
+    func testNoosphereReset() async throws {
         let base = UUID()
         
         let globalStorageURL = try Self.createTmpDir("\(base)/noosphere")
@@ -75,40 +84,40 @@ final class Tests_NoosphereService: XCTestCase {
             sphereStorageURL: sphereStorageURL
         )
 
-        let receipt = try noosphere.createSphere(ownerKeyName: "bob")
-        noosphere.resetSphere(receipt.identity)
+        let receipt = try await noosphere.createSphere(ownerKeyName: "bob")
+        await noosphere.resetSphere(receipt.identity)
 
-        let versionA = try noosphere.version()
+        let versionA = try await noosphere.version()
         
         let body = try "Test content".toData().unwrap()
-        try noosphere.write(
+        try await noosphere.write(
             slug: Slug("a")!,
             contentType: "text/subtext",
             additionalHeaders: [],
             body: body
         )
-        let versionB = try noosphere.save()
+        let versionB = try await noosphere.save()
         XCTAssertNotEqual(versionA, versionB)
 
-        try noosphere.write(
+        try await noosphere.write(
             slug: Slug("b")!,
             contentType: "text/subtext",
             additionalHeaders: [],
             body: body
         )
-        try noosphere.write(
+        try await noosphere.write(
             slug: Slug("c")!,
             contentType: "text/subtext",
             additionalHeaders: [],
             body: body
         )
-        let versionC = try noosphere.save()
+        let versionC = try await noosphere.save()
         XCTAssertNotEqual(versionB, versionC)
 
         // Clear out Noosphere and Sphere instance
-        noosphere.reset()
+        await noosphere.reset()
 
-        let versionZ = try noosphere.version()
+        let versionZ = try await noosphere.version()
         XCTAssertEqual(versionC, versionZ)
     }
 }
