c604adb6cb5fc27565011e2751389530a4a48ada
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-04-18 14:33:05 +1000

    Combine all FollowUserForm code into one file

--- xcode/Subconscious/Shared/Components/Form/FollowUserFormView.swift
+++ xcode/Subconscious/Shared/Components/Form/FollowUserFormView.swift
@@ -153,3 +153,77 @@ struct FollowUserView_Previews: PreviewProvider {
         )
     }
 }
+
+// MARK: Actions
+
+enum FollowUserFormAction: Equatable {
+    case didField(FormFieldAction<String>)
+    case petnameField(FormFieldAction<String>)
+}
+
+// MARK: Model
+
+struct FollowUserFormModel: ModelProtocol {
+    typealias Action = FollowUserFormAction
+    typealias Environment = ()
+    
+    var did: FormField<String, Did> = FormField(value: "", validate: Self.validateDid)
+    var petname: FormField<String, Petname> = FormField(value: "", validate: Self.validatePetname)
+    
+    static func validateDid(key: String) -> Did? {
+        Did(key)
+    }
+
+    static func validatePetname(petname: String) -> Petname? {
+        Petname(petname)
+    }
+    
+    static func update(state: Self, action: Action, environment: Environment) -> Update<Self> {
+        switch action {
+        case .didField(let action):
+            return DidFieldCursor.update(state: state, action: action, environment: FormFieldEnvironment())
+        case .petnameField(let action):
+            return PetnameFieldCursor.update(state: state, action: action, environment: FormFieldEnvironment())
+        }
+    }
+}
+
+// MARK: Cursors
+
+struct PetnameFieldCursor: CursorProtocol {
+    typealias Model = FollowUserFormModel
+    typealias ViewModel = FormField<String, Petname>
+
+    static func get(state: Model) -> ViewModel {
+        state.petname
+    }
+    
+    static func set(state: Model, inner: ViewModel) -> Model {
+        var model = state
+        model.petname = inner
+        return model
+    }
+    
+    static func tag(_ action: ViewModel.Action) -> Model.Action {
+        FollowUserFormAction.petnameField(action)
+    }
+}
+
+struct DidFieldCursor: CursorProtocol {
+    typealias Model = FollowUserFormModel
+    typealias ViewModel = FormField<String, Did>
+
+    static func get(state: Model) -> ViewModel {
+        state.did
+    }
+    
+    static func set(state: Model, inner: ViewModel) -> Model {
+        var model = state
+        model.did = inner
+        return model
+    }
+    
+    static func tag(_ action: ViewModel.Action) -> Model.Action {
+        FollowUserFormAction.didField(action)
+    }
+}
