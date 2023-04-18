9d963cadf58f8ae9187c9202ef874fac37f2093f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-04-18 14:45:20 +1000

    Update tests

--- xcode/Subconscious/SubconsciousTests/Tests_AddressBookService.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_AddressBookService.swift
@@ -9,48 +9,48 @@ import XCTest
 @testable import Subconscious
 
 final class Tests_AddressBookService: XCTestCase {
-    func testFollowUser() throws {
+    func testFollowUser() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
-        let entries = try addressBook.listEntries()
+        let entries = try await addressBook.listEntries()
         XCTAssertEqual(entries, [])
         
         let did = Did("did:key:123")!
         let petname = Petname("ziggy")!
-        try addressBook.followUser(did: did, petname: petname)
+        try await addressBook.followUser(did: did, petname: petname)
         
-        let newEntries = try addressBook.listEntries()
+        let newEntries = try await addressBook.listEntries()
         let user = newEntries[0]
         
         XCTAssertEqual(user.did, did)
         XCTAssertEqual(user.petname, petname)
     }
     
-    func testUnfollowByPetname() throws {
+    func testUnfollowByPetname() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
-        let entries = try addressBook.listEntries()
+        let entries = try await addressBook.listEntries()
         XCTAssertEqual(entries, [])
         
         let did = Did("did:key:123")!
         let petname = Petname("ziggy")!
-        try addressBook.followUser(did: did, petname: petname)
+        try await addressBook.followUser(did: did, petname: petname)
         
         let did2 = Did("did:key:456")!
         let petname2 = Petname("flubbo")!
-        try addressBook.followUser(did: did2, petname: petname2)
+        try await addressBook.followUser(did: did2, petname: petname2)
         
-        let newEntries = try addressBook.listEntries()
+        let newEntries = try await addressBook.listEntries()
         
         XCTAssertEqual(newEntries.count, 2)
         
-        try addressBook.unfollowUser(petname: petname)
+        try await addressBook.unfollowUser(petname: petname)
         
-        let finalEntries = try addressBook.listEntries()
+        let finalEntries = try await addressBook.listEntries()
         let user = finalEntries[0]
         
         XCTAssertEqual(finalEntries.count, 1)
@@ -58,29 +58,29 @@ final class Tests_AddressBookService: XCTestCase {
         XCTAssertEqual(user.petname, petname2)
     }
     
-    func testUnfollowByDid() throws {
+    func testUnfollowByDid() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
-        let entries = try addressBook.listEntries()
+        let entries = try await addressBook.listEntries()
         XCTAssertEqual(entries, [])
         
         let did = Did("did:key:123")!
         let petname = Petname("ziggy")!
-        try addressBook.followUser(did: did, petname: petname)
+        try await addressBook.followUser(did: did, petname: petname)
         
         let did2 = Did("did:key:456")!
         let petname2 = Petname("flubbo")!
-        try addressBook.followUser(did: did2, petname: petname2)
+        try await addressBook.followUser(did: did2, petname: petname2)
         
-        let newEntries = try addressBook.listEntries()
+        let newEntries = try await addressBook.listEntries()
         
         XCTAssertEqual(newEntries.count, 2)
         
-        try addressBook.unfollowUser(did: did)
+        try await addressBook.unfollowUser(did: did)
         
-        let finalEntries = try addressBook.listEntries()
+        let finalEntries = try await addressBook.listEntries()
         let user = finalEntries[0]
         
         XCTAssertEqual(finalEntries.count, 1)
@@ -88,84 +88,65 @@ final class Tests_AddressBookService: XCTestCase {
         XCTAssertEqual(user.petname, petname2)
     }
     
-    func testFindAvailablePetname() throws {
+    func testFindAvailablePetname() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
-        let entries = try addressBook.listEntries()
+        let entries = try await addressBook.listEntries()
         XCTAssertEqual(entries, [])
         
         let did = Did("did:key:123")!
         let petname = Petname("ziggy")!
-        try addressBook.followUser(did: did, petname: petname)
+        try await addressBook.followUser(did: did, petname: petname)
         
-        let newPetname = try addressBook.findAvailablePetname(petname: Petname("ziggy")!)
+        let newPetname = try await addressBook.findAvailablePetname(petname: Petname("ziggy")!)
         XCTAssertEqual(newPetname, Petname("ziggy-1")!)
     }
     
-    func testFindAvailablePetnameWithExistingSuffix() throws {
+    func testFindAvailablePetnameWithExistingSuffix() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
-        let entries = try addressBook.listEntries()
+        let entries = try await addressBook.listEntries()
         XCTAssertEqual(entries, [])
         
         for i in 0..<64 {
             let did = Did("did:key:123\(i)")!
             let petname = Petname("ziggy-\(i)")!
-            try addressBook.followUser(did: did, petname: petname)
+            try await addressBook.followUser(did: did, petname: petname)
         }
         
-        let newPetname = try addressBook.findAvailablePetname(petname: Petname("ziggy-1")!)
+        let newPetname = try await addressBook.findAvailablePetname(petname: Petname("ziggy-1")!)
         XCTAssertEqual(newPetname, Petname("ziggy-64")!)
     }
     
-    func testIsFollowingUser() throws {
+    func testIsFollowingUserAndHasEntryFor() async throws {
         let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
+        let data = try await TestUtilities.createDataServiceEnvironment(tmp: tmp)
         let addressBook = data.addressBook
         
         let did = Did("did:key:123")!
         let petname = Petname("ziggy-2")!
         
-        let a = try addressBook.isFollowingUser(did: did)
-        let b = addressBook.isFollowingUser(did: did, petname: petname)
+        let a = try await addressBook.isFollowingUser(did: did)
+        let b = await addressBook.hasEntryForPetname(petname: petname)
         XCTAssertFalse(a)
         XCTAssertFalse(b)
     
-        try addressBook.followUser(did: did, petname: petname)
+        try await addressBook.followUser(did: did, petname: petname)
         
-        let c = try addressBook.isFollowingUser(did: did)
-        let d = addressBook.isFollowingUser(did: did, petname: petname)
+        let c = try await addressBook.isFollowingUser(did: did)
+        let d = await addressBook.hasEntryForPetname(petname: petname)
         XCTAssertTrue(c)
         XCTAssertTrue(d)
         
-        try addressBook.unfollowUser(did: did)
+        try await addressBook.unfollowUser(did: did)
         
-        let e = try addressBook.isFollowingUser(did: did)
-        let f = addressBook.isFollowingUser(did: did, petname: petname)
+        let e = try await addressBook.isFollowingUser(did: did)
+        let f = await addressBook.hasEntryForPetname(petname: petname)
         XCTAssertFalse(e)
         XCTAssertFalse(f)
-    }
-    
-    func testHasEntryFor() throws {
-        let tmp = try TestUtilities.createTmpDir()
-        let data = try TestUtilities.createDataService(tmp: tmp)
-        let addressBook = data.addressBook
-        
-        let did = Did("did:key:123")!
-        let petname = Petname("ziggy-2")!
-        
-        XCTAssertFalse(addressBook.hasEntryForPetname(petname: petname))
-        
-        try addressBook.followUser(did: did, petname: petname)
-        
-        XCTAssertTrue(addressBook.hasEntryForPetname(petname: petname))
-        
-        try addressBook.unfollowUser(did: did)
-        
-        XCTAssertFalse(addressBook.hasEntryForPetname(petname: petname))
     }
 }
