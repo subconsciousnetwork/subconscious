d65bfbbb4ee4aca04f8032da0403db8f3c3f5f69
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-04-28 11:37:35 +1000

    Follow subconscious geist on sphere creation

--- xcode/Subconscious/Shared/Config.swift
+++ xcode/Subconscious/Shared/Config.swift
@@ -29,6 +29,8 @@ struct Config: Equatable, Codable {
             debug ? "did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7" : ""
         }
     }
+    var subconsciousGeistDid: Did = Did("did:key:z6MkmCJAZansQ3p1Qwx6wrF4c64yt2rcM8wMrH5Rh7DGb2K7")!
+    var subconsciousGeistPetname: Petname = Petname("subconscious")!
 
     /// Standard interval at which to run long-polling services
     var pollingInterval: Double = 15
