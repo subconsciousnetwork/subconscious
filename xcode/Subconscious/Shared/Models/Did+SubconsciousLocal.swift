fb279f12532d74878d56d320b525cc951148a282
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-05-02 13:33:39 -0400

    Add isLocal extension

--- xcode/Subconscious/Shared/Models/Did+SubconsciousLocal.swift
+++ xcode/Subconscious/Shared/Models/Did+SubconsciousLocal.swift
@@ -9,3 +9,16 @@ extension Did {
     /// A non-standard did we use to represent the local file system.
     static let local = Did("did:subconscious:local")
 }
+
+extension Slashlink {
+    /// Does this slashlink point to content saved to the local file system,
+    /// rather than a sphere?
+    var isLocal: Bool {
+        switch self.peer {
+        case .did(let did):
+            return did == Did.local
+        default:
+            return false
+        }
+    }
+}
