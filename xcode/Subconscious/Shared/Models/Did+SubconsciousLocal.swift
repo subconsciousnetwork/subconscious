330ce31fe42a5284ac9958eed645017350161b70
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-05-02 13:44:40 -0400

    Add isOurs

--- xcode/Subconscious/Shared/Models/Did+SubconsciousLocal.swift
+++ xcode/Subconscious/Shared/Models/Did+SubconsciousLocal.swift
@@ -18,22 +18,38 @@ extension Slashlink {
     /// rather than a sphere?
     var isLocal: Bool {
         switch self.peer {
-        case .did(let did):
-            return did == Did.local
+        case .did(let did) where did == Did.local:
+            return true
         default:
             return false
         }
     }
-
+    
     /// Get audience from slashlink
     func toAudience() -> Audience {
         switch self.peer {
-        case .did(let did):
-            return did == Did.local ? .local : .public
+        case .did(let did) where did == Did.local:
+            return .local
+        case .did:
+            return .public
         case .petname:
             return .public
         case .none:
             return .public
+        }
+    }
+    
+    /// Check if slashlink points to content that belongs to us.
+    var isOurs: Bool {
+        switch self.peer {
+        case .did(let did) where did == Did.local:
+            return true
+        case .did:
+            return false
+        case .petname:
+            return false
+        case .none:
+            return true
         }
     }
 }
