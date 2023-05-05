50c45674cb2dfebca94ef2cb2ceb4872065f4788
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-05-05 14:03:13 -0400

    Better handling for profile slashlinks

--- xcode/Subconscious/Shared/Components/Common/SlashlinkDisplayView.swift
+++ xcode/Subconscious/Shared/Components/Common/SlashlinkDisplayView.swift
@@ -12,6 +12,7 @@ struct SlashlinkDisplayView: View {
     var slashlink: Slashlink
     var baseColor = Color.accentColor
     var slugColor = Color.accentColor
+    var labelColor = Color.secondary
     
     var body: some View {
         HStack(spacing: 0) {
@@ -43,6 +44,9 @@ struct SlashlinkDisplayView: View {
                     Text(verbatim: slashlink.slug.markup)
                         .foregroundColor(slugColor)
                 }
+            case .none where slashlink.isProfile:
+                Text("Your profile")
+                    .foregroundColor(labelColor)
             case .none:
                 Text(verbatim: slashlink.slug.description)
                     .foregroundColor(slugColor)
@@ -53,11 +57,13 @@ struct SlashlinkDisplayView: View {
     
     func theme(
         base baseColor: Color = Color.accentColor,
-        slug slugColor: Color = Color.accentColor
+        slug slugColor: Color = Color.accentColor,
+        label labelColor: Color = Color.secondary
     ) -> Self {
         var this = self
         this.baseColor = baseColor
         this.slugColor = slugColor
+        this.labelColor = labelColor
         return this
     }
 }
@@ -65,33 +71,50 @@ struct SlashlinkDisplayView: View {
 struct SlashlinkDisplayView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            SlashlinkDisplayView(
-                slashlink: Slashlink("/foo")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("/foo")!
-            ).foregroundColor(.accentColor)
-            SlashlinkDisplayView(
-                slashlink: Slashlink("/foo/bar")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("@alice/foo/bar")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("@bob.alice/foo/bar")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("did:key:abc123/foo/bar")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("did:key:abc123")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("did:subconscious:local/foo")!
-            )
-            SlashlinkDisplayView(
-                slashlink: Slashlink("did:subconscious:local")!
-            )
+            Group {
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("/foo")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("/foo")!
+                ).foregroundColor(.accentColor)
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("/foo/bar")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("/_profile_")!
+                )
+            }
+            Group {
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("@alice/foo/bar")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("@bob.alice/foo/bar")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("@bob.alice/_profile_")!
+                )
+            }
+            Group {
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("did:key:abc123/foo/bar")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("did:key:abc123")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("did:key:abc123/_profile_")!
+                )
+            }
+            Group {
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("did:subconscious:local/foo")!
+                )
+                SlashlinkDisplayView(
+                    slashlink: Slashlink("did:subconscious:local")!
+                )
+            }
         }
     }
 }
