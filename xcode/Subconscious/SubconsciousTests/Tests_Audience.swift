e1bc111e35203d69bc06d064beba805f97e81297
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-05-10 14:57:39 -0400

    Introduce userDescription to Audience
    Provides a user-facing description of the type.
    
    Also implement LosslessStringConvertible for type.

---
+++ xcode/Subconscious/SubconsciousTests/Tests_Audience.swift
@@ -0,0 +1,27 @@
+//
+//  Tests_Audience.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 5/10/23.
+//
+
+import XCTest
+@testable import Subconscious
+
+final class Tests_Audience: XCTestCase {
+    func testAudienceUserDescription() throws {
+        let local = Audience.local
+        XCTAssertEqual(local.userDescription, "Draft")
+        
+        let `public` = Audience.public
+        XCTAssertEqual(`public`.userDescription, "Public")
+    }
+    
+    func testAudienceLosslessStringConvertible() throws {
+        let local = Audience("local")
+        XCTAssertEqual(local, Audience.local)
+        
+        let `public` = Audience("public")
+        XCTAssertEqual(`public`, Audience.public)
+    }
+}
