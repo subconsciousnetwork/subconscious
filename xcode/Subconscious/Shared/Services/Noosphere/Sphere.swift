6fb108ca0bc27ed9835d87a2806dc0c30845a002
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-05-12 11:02:04 +1000

    Correctly resolve second-order(+) petnames
    getPetname is a footgun, it only looks in the sphere's address book.

--- xcode/Subconscious/Shared/Services/Noosphere/Sphere.swift
+++ xcode/Subconscious/Shared/Services/Noosphere/Sphere.swift
@@ -74,7 +74,8 @@ extension SphereProtocol {
             return slashlink
         case .petname(let petname):
             // Get did for petname
-            let did = try await self.getPetname(petname: petname)
+            let sphere = try await self.traverse(petname: petname)
+            let did = try await sphere.identity()
             // Return new slashlink with did root
             return Slashlink(
                 peer: .did(did),
