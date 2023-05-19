7173849579d8c5c09d7f412159ff0a3ca710c37a
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-05-19 10:14:55 +1000

    Create sphere _after_ first step
    We need the invite code before we can get started

--- xcode/Subconscious/Shared/Components/FirstRun/FirstRunProfileView.swift
+++ xcode/Subconscious/Shared/Components/FirstRun/FirstRunProfileView.swift
@@ -69,6 +69,9 @@ struct FirstRunProfileView: View {
             AppTheme.onboarding
                 .appBackgroundGradient(colorScheme)
         )
+        .onAppear {
+            app.send(.createSphere)
+        }
     }
 }
 
