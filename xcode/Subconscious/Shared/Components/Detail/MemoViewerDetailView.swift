4fbd5d191e79cd1f60c51be0224182051bcbac98
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-06-29 17:57:09 +1000

    Transclude blocks: fast follow fixes (#762)
    Since we merged
    https://github.com/subconsciousnetwork/subconscious/pull/574 for
    playtesting this PR addresses the formatting and small fixups from
    @gordonbrander's review.

--- xcode/Subconscious/Shared/Components/Detail/MemoViewerDetailView.swift
+++ xcode/Subconscious/Shared/Components/Detail/MemoViewerDetailView.swift
@@ -429,15 +429,18 @@ struct MemoViewerDetailModel: ModelProtocol {
     ) -> Update<MemoViewerDetailModel> {
         
         guard let owner = state.owner else {
-            return Update(state: state)
+            return update(
+                state: state,
+                action: .failFetchTranscludePreviews("Owner profile is not loaded"),
+                environment: environment
+            )
         }
         
         let links = state.dom.slashlinks
-            .map { value in value.toSlashlink() }
-            .compactMap { value in value }
+            .compactMap { value in value.toSlashlink() }
         
         let fx: Fx<MemoViewerDetailAction> =
-        environment.transclude
+            environment.transclude
             .fetchTranscludePreviewsPublisher(slashlinks: links, owner: owner)
             .map { entries in
                 MemoViewerDetailAction.succeedFetchTranscludePreviews(entries)
