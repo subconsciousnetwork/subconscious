c4d8f0ff1d0fe3bda1d27d3b62f9230f6b5bc707
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-08-28 15:46:46 -0400

    Introduce ControllerStore
    ...along with basic unit test. ControllerStore is the store primitive we
    use to drive mutable UIKit code.

---
+++ xcode/Subconscious/SubconsciousTests/Tests_ControllerStore.swift
@@ -0,0 +1,77 @@
+//
+//  Tests_ControllerStore.swift
+//  SubconsciousTests
+//
+//  Created by Gordon Brander on 8/28/23.
+//
+
+import XCTest
+import UIKit
+@testable import Subconscious
+
+final class Tests_ControllerStore: XCTestCase {
+    enum TestAction: Hashable {
+        case setText(String)
+    }
+
+    struct TestModel: Hashable {
+        var text: String = ""
+    }
+
+    class TestViewController: UIViewController, ControllerStoreControllerProtocol {
+        typealias Model = TestModel
+        typealias Action = TestAction
+        
+        lazy var textView = UITextView(frame: .zero)
+
+        override func viewDidLoad() {
+            view.addSubview(textView)
+            NSLayoutConstraint.activate([
+                textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                textView.topAnchor.constraint(equalTo: view.topAnchor),
+                textView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
+        }
+
+        @MainActor
+        func reconfigure(
+            state: TestModel,
+            send: @escaping (TestAction) -> Void
+        ) {
+            self.textView.text = state.text
+        }
+
+        @MainActor
+        func update(
+            state: TestModel,
+            action: TestAction
+        ) -> Update {
+            switch action {
+            case .setText(let text):
+                return setText(state: state, text: text)
+            }
+        }
+
+        func setText(
+            state: TestModel,
+            text: String
+        ) -> Update {
+            let render = {
+                self.textView.text = text
+            }
+            return Update(state: state, render: render)
+        }
+    }
+
+    @MainActor
+    func testRunsRender() throws {
+        var store = ControllerStore.Store<TestViewController>(
+            state: TestModel()
+        )
+        var controller = TestViewController()
+        store.connect(controller)
+        store.transact(TestAction.setText("Foo"))
+        XCTAssertEqual(controller.textView.text, "Foo", "Store renders text")
+    }
+}
