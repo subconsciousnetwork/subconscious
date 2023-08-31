946870865914b23f47c17a5775941bbb882255f9
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-08-31 09:50:33 -0400

    Stub out Transclude view
    ...and move SubtextEditorMarkup so name matches convention.

---
+++ xcode/Subconscious/Shared/Components/BlockEditor/BlockEditorTranscludeView.swift
@@ -0,0 +1,43 @@
+//
+//  BlockEditorTranscludeView.swift
+//  Subconscious (iOS)
+//
+//  Created by Gordon Brander on 8/31/23.
+//
+
+import UIKit
+
+extension BlockEditor {
+    class TranscludeView: UIView, Identifiable {
+        var id = UUID()
+        private lazy var stackView = UIStackView(frame: .zero)
+        private lazy var addressView = UILabel(frame: .zero)
+        private lazy var excerptView = UILabel(frame: .zero)
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            stackView.axis = .vertical
+            stackView.addArrangedSubview(addressView)
+            stackView.addArrangedSubview(excerptView)
+            addSubview(stackView)
+
+            NSLayoutConstraint.activate([
+                stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                stackView.topAnchor.constraint(equalTo: topAnchor),
+                self.bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
+            ])
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func render(
+            _ stub: EntryStub
+        ) {
+            self.addressView.text = stub.address.description
+            self.excerptView.text = stub.excerpt
+        }
+    }
+}
