c60e8b8358c419886c2df2dbb68e88502459dded
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-08-31 10:11:37 -0400

    Stub out related view

--- xcode/Subconscious/Shared/Components/BlockEditor/BlockEditorRelatedView.swift
+++ xcode/Subconscious/Shared/Components/BlockEditor/BlockEditorRelatedView.swift
@@ -10,18 +10,28 @@ import UIKit
 extension BlockEditor {
     class RelatedView: UIView, Identifiable {
         var id = UUID()
+        private lazy var label = UILabel()
         private lazy var stackView = UIStackView(frame: .zero)
 
         override init(frame: CGRect) {
             super.init(frame: frame)
+            
+            label.text = String(localized: "Related notes")
+            label.font = UIFont.preferredFont(forTextStyle: .caption2)
+            label.numberOfLines = 2
+            addSubview(label)
+            
             stackView.axis = .vertical
             addSubview(stackView)
             
             NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: leadingAnchor),
+                label.trailingAnchor.constraint(equalTo: trailingAnchor),
+                label.topAnchor.constraint(equalTo: topAnchor),
                 stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
                 stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-                stackView.topAnchor.constraint(equalTo: topAnchor),
-                self.bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
+                stackView.topAnchor.constraint(equalTo: label.topAnchor),
+                bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
             ])
         }
         
@@ -33,6 +43,11 @@ extension BlockEditor {
             _ related: RelatedModel
         ) {
             stackView.removeAllArrangedSubviews()
+            for stub in related.related {
+                let transclude = TranscludeView(frame: .zero)
+                transclude.render(stub)
+                stackView.addArrangedSubview(transclude)
+            }
         }
     }
 }
