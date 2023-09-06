c1534eb54fb626bd94de8c31e420627cb19e12ff
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-09-06 12:58:15 -0400

    Add spacing

--- xcode/Subconscious/Shared/UIKit/BlockEditor/BlockEditorRelatedView.swift
+++ xcode/Subconscious/Shared/UIKit/BlockEditor/BlockEditorRelatedView.swift
@@ -10,6 +10,8 @@ import UIKit
 extension BlockEditor {
     class RelatedView: UIView, UIComponentViewProtocol {
         var id = UUID()
+        private var bodySpacing: CGFloat = 8
+        private var transcludeSpacing: CGFloat = 8
         private lazy var label = UILabel()
         private lazy var bodyView = UIStackView(frame: .zero)
         private lazy var transcludesView = UIStackView(frame: .zero)
@@ -18,6 +20,7 @@ extension BlockEditor {
             super.init(frame: frame)
             
             bodyView.axis = .vertical
+            bodyView.spacing = bodySpacing
             bodyView.translatesAutoresizingMaskIntoConstraints = false
             addSubview(bodyView)
             
@@ -33,6 +36,7 @@ extension BlockEditor {
             
             transcludesView.axis = .vertical
             transcludesView.translatesAutoresizingMaskIntoConstraints = false
+            transcludesView.spacing = transcludeSpacing
             bodyView.addArrangedSubview(transcludesView)
             
             NSLayoutConstraint.activate([
