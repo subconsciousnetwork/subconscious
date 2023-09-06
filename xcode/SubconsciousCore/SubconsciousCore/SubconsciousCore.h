ffc25a82883be9fed1c1162ce5087b4effd6f22c
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-09-06 11:10:13 -0400

    Create workspace and SubconsciousCore framework

---
+++ xcode/SubconsciousCore/SubconsciousCore/SubconsciousCore.h
@@ -0,0 +1,18 @@
+//
+//  SubconsciousCore.h
+//  SubconsciousCore
+//
+//  Created by Gordon Brander on 9/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubconsciousCore.
+FOUNDATION_EXPORT double SubconsciousCoreVersionNumber;
+
+//! Project version string for SubconsciousCore.
+FOUNDATION_EXPORT const unsigned char SubconsciousCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubconsciousCore/PublicHeader.h>
+
+
