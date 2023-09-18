2fe1891378beb15f14661f54818c392f72bc73d5
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-09-18 11:58:44 +1000

    Implement wizard steps

--- xcode/Subconscious/Shared/RecoveryView.swift
+++ xcode/Subconscious/Shared/RecoveryView.swift
@@ -46,89 +46,136 @@ struct AttemptRecoveryLabel: View {
     }
 }
 
+enum RecoveryViewTab {
+    case explain
+    case form
+}
 
 struct RecoveryView: View {
     @ObservedObject var app: Store<AppModel>
+    @State var selectedTab = RecoveryViewTab.explain // TODO: move to a small, local store
     
     var body: some View {
-        NavigationStack {
+        TabView(selection: $selectedTab) {
+            VStack(spacing: AppTheme.padding) {
+                Text("Recovery Mode")
+                    .bold()
+                    .padding(.bottom)
+                
+                Spacer()
+                
+                Image(systemName: "stethoscope")
+                    .resizable()
+                    .frame(width: 96, height: 96)
+                    .foregroundColor(.secondary)
+                
+                Text(
+                    "If you ever lose your device or experience data loss,"
+                     + " you can recover your data using your recovery phrase and your gateway."
+                )
+                .multilineTextAlignment(.center)
+                
+                Text(
+                    "We'll attempt to download and restore from the remote copy of your notes."
+                )
+                .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Button(
+                    action: {
+                        withAnimation {
+                            selectedTab = .form
+                        }
+                    },
+                    label: {
+                        Text("Proceed")
+                    }
+                )
+                .buttonStyle(PillButtonStyle())
+                
+                Button(
+                    action: {
+                        app.send(.presentRecoveryMode(false))
+                    },
+                    label: {
+                        Text("Cancel")
+                    }
+                )
+                
+                Spacer()
+            }
+            .padding(AppTheme.padding)
+            .tabItem {
+                Text("Recovery")
+            }
+            .tag(RecoveryViewTab.explain)
+            
             Form {
                 Section(
                     content: {
-                    ValidatedFormField(
-                        placeholder: "did:key:abc",
-                        field: app.state.recoveryDidField,
-                        send: Address.forward(
-                            send: app.send,
-                            tag: AppAction.recoveryDidField
-                        ),
-                        caption: "The identity of your sphere"
-                    )
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    
-                    ValidatedFormField(
-                        placeholder: "http://example.com",
-                        field: app.state.gatewayURLField,
-                        send: Address.forward(
-                            send: app.send,
-                            tag: AppAction.gatewayURLField
-                        ),
-                        caption: String(localized: "The URL of your preferred Noosphere gateway")
-                    )
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    .autocapitalization(.none)
-                    .keyboardType(.URL)
-                    .onDisappear {
-                        app.send(.submitGatewayURLForm)
-                    }
-                    .disabled(app.state.gatewayOperationInProgress)
-                    
-                    ValidatedFormField(
-                        placeholder: "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty-one twenty-two tenty-three twenty-four",
-                        field: app.state.recoveryPhraseField,
-                        send: Address.forward(
-                            send: app.send,
-                            tag: AppAction.recoveryPhraseField
-                        ),
-                        caption: "Recovery phrase",
-                        axis: .vertical
-                    )
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
+                        ValidatedFormField(
+                            placeholder: "did:key:abc",
+                            field: app.state.recoveryDidField,
+                            send: Address.forward(
+                                send: app.send,
+                                tag: AppAction.recoveryDidField
+                            ),
+                            caption: "The identity of your sphere"
+                        )
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
                         
-                    Button(
-                        action: {
-                            app.send(.requestRecovery)
-                        },
-                        label: {
-                            AttemptRecoveryLabel(status: app.state.recoveryStatus)
+                        ValidatedFormField(
+                            placeholder: "http://example.com",
+                            field: app.state.gatewayURLField,
+                            send: Address.forward(
+                                send: app.send,
+                                tag: AppAction.gatewayURLField
+                            ),
+                            caption: String(localized: "The URL of your preferred Noosphere gateway")
+                        )
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .keyboardType(.URL)
+                        .onDisappear {
+                            app.send(.submitGatewayURLForm)
                         }
-                    )
-                }, header: {
-                    Text("Recovery")
-                }, footer: {
-                    Text("If you ever lose your device or delete the application data, you can recover your data using your recovery phrase and your gateway.")
-                })
-                
-                
-                
+                        .disabled(app.state.gatewayOperationInProgress)
+                        
+                        ValidatedFormField(
+                            placeholder: "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty-one twenty-two tenty-three twenty-four",
+                            field: app.state.recoveryPhraseField,
+                            send: Address.forward(
+                                send: app.send,
+                                tag: AppAction.recoveryPhraseField
+                            ),
+                            caption: "Recovery phrase",
+                            axis: .vertical
+                        )
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                        
+                        Button(
+                            action: {
+                                app.send(.requestRecovery)
+                            },
+                            label: {
+                                AttemptRecoveryLabel(status: app.state.recoveryStatus)
+                            }
+                        )
+                    },
+                    header: {
+                        Text("Recovery")
+                    }
+                )
             }
-            .navigationTitle("Recovery")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .cancellationAction) {
-                    Button(
-                        action: {
-                            app.send(.presentRecovery(false))
-                        },
-                        label: {
-                            Text("Cancel")
-                        }
-                    )
-                }
+            .tabItem {
+                Text("Form")
             }
+            .tag(RecoveryViewTab.form)
         }
+        .tabViewStyle(.page)
     }
 }
