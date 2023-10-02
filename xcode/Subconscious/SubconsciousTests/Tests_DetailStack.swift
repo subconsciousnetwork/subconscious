fa14a9374219b4a8879bdfc1b63b381ab70ee06f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-10-02 11:40:05 +1000

    Introduce tests for merge, move, update audience and delete

--- xcode/Subconscious/SubconsciousTests/Tests_DetailStack.swift
+++ xcode/Subconscious/SubconsciousTests/Tests_DetailStack.swift
@@ -66,4 +66,117 @@ class Tests_DetailStack: XCTestCase {
             return
         }
     }
+    
+    func testSucceedDeleteMemo() throws {
+        let address = Slashlink("/test")!
+        
+        let state = DetailStackModel(details: [
+            MemoDetailDescription.from(address: address, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-2")!, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-3")!, fallback: ""),
+        ])
+
+        let update = DetailStackModel.update(
+            state: state,
+            action: .succeedDeleteMemo(address),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.details.count,
+            2,
+            "Removes deleted detail"
+        )
+        
+        XCTAssertEqual(update.state.details[0].address, Slashlink("/test-2")!)
+        XCTAssertEqual(update.state.details[1].address, Slashlink("/test-3")!)
+    }
+    
+    func testSucceedUpdateAudience() throws {
+        let address = Slashlink("/test")!
+        let updatedAddress = Slashlink(peer: .did(Did.local), slug: Slug("test")!)
+        
+        let state = DetailStackModel(details: [
+            MemoDetailDescription.from(address: address, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-2")!, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-3")!, fallback: ""),
+        ])
+
+        let update = DetailStackModel.update(
+            state: state,
+            action: .succeedUpdateAudience(
+                MoveReceipt(
+                    from: address,
+                    to: updatedAddress
+                )
+            ),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.details.count,
+            3,
+            "Preserves details"
+        )
+        
+        // audience is now local
+        XCTAssertEqual(update.state.details[0].address, updatedAddress)
+        XCTAssertEqual(update.state.details[1].address, Slashlink("/test-2")!)
+        XCTAssertEqual(update.state.details[2].address, Slashlink("/test-3")!)
+    }
+    
+    func testSucceedMoveEntry() throws {
+        let state = DetailStackModel(details: [
+            MemoDetailDescription.from(address: Slashlink("/test")!, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-2")!, fallback: ""),
+            MemoDetailDescription.from(address: Slashlink("/test-3")!, fallback: ""),
+        ])
+
+        let address = Slashlink("/test-3")!
+        let updatedAddress = Slashlink("/foo")!
+
+        let update = DetailStackModel.update(
+            state: state,
+            action: .succeedMoveEntry(from: address, to: updatedAddress),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.details.count,
+            3,
+            "Preserves details"
+        )
+        
+        XCTAssertEqual(update.state.details[0].address, Slashlink("/test")!)
+        XCTAssertEqual(update.state.details[1].address, Slashlink("/test-2")!)
+        // test-3 has become foo
+        XCTAssertEqual(update.state.details[2].address, updatedAddress)
+    }
+    
+    func testSucceedMergeEntry() throws {
+        let address = Slashlink("/test-3")!
+        let parent = Slashlink("/test-2")!
+        
+        let state = DetailStackModel(details: [
+            MemoDetailDescription.from(address: Slashlink("/test")!, fallback: ""),
+            MemoDetailDescription.from(address: parent, fallback: ""),
+            MemoDetailDescription.from(address: address, fallback: ""),
+        ])
+
+        let update = DetailStackModel.update(
+            state: state,
+            action: .succeedMergeEntry(
+                parent: parent,
+                child: address
+            ),
+            environment: environment
+        )
+        XCTAssertEqual(
+            update.state.details.count,
+            3,
+            "Preserves details"
+        )
+        
+        XCTAssertEqual(update.state.details[0].address, Slashlink("/test")!)
+        // test-3 has been turned into test-2
+        XCTAssertEqual(update.state.details[1].address, parent)
+        XCTAssertEqual(update.state.details[2].address, parent)
+    }
 }
