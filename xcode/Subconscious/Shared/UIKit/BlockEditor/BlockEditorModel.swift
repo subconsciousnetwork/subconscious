89fdfe58a217af9582a6c572ae494a62f2024965
Gordon Brander <gordonbrander@users.noreply.github.com>
2023-10-31 15:13:20 -0400

    Add helper for finding blocks

--- xcode/Subconscious/Shared/UIKit/BlockEditor/BlockEditorModel.swift
+++ xcode/Subconscious/Shared/UIKit/BlockEditor/BlockEditorModel.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
+import os
 
 extension BlockEditor {
     //  MARK: Model
     struct Model: Hashable {
+        static let logger = Logger(
+            subsystem: Config.default.rdns,
+            category: "BlockEditor.Model"
+        )
+
         /// Create a BlockEditor "draft" document.
         /// A draft is a document with a heading block and nothing else.
         static func draft() -> Self {
@@ -23,5 +29,13 @@ extension BlockEditor {
         var isBlockSelectMode = false
         var blocks: [BlockModel] = []
         var appendix = RelatedModel()
+
+        func block(id: UUID) -> Array.Index? {
+            guard let i = blocks.firstIndex(whereID: id) else {
+                Self.logger.log("block#\(id) not found.")
+                return nil
+            }
+            return i
+        }
     }
 }
