d600e5311343cc50c7fbf9bdbfff554e281c68de
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-11-15 16:49:04 +1000

    Hash based colours

--- xcode/Subconscious/CardStackView.swift
+++ xcode/Subconscious/CardStackView.swift
@@ -4,13 +4,41 @@ struct CardView: View {
     var entry: EntryStub
 //    var t: CGFloat
     
+    static let colors: [Color] = [
+        Color(red: 0.97, green: 0.49, blue: 0.75),
+        Color(red: 0.56, green: 0.62, blue: 0.93),
+        Color(red: 0.93, green: 0.59, blue: 0.56),
+        Color(red: 0.74, green: 0.52, blue: 0.95),
+        Color(red: 0.97, green: 0.75, blue: 0.48)
+    ]
+    var color: Color {
+        Self.colors[abs(entry.hashValue) % Self.colors.count]
+    }
+    
     var body: some View {
-        TranscludeView(entry: entry, onRequestDetail: { }, onLink: { _ in })
-            .allowsHitTesting(false)
-            .frame(width: 300, height: 400)
-            .background(Color.tertiarySystemGroupedBackground)
-            .cornerRadius(10)
-            .shadow(color: Color.black.opacity(0.1), radius: 5)
+        VStack(alignment: .leading, spacing: AppTheme.unit2) {
+            BylineSmView(
+                pfp: .generated(entry.did),
+                slashlink: entry.address
+            )
+            
+            SubtextView(subtext: entry.excerpt)
+                .environment(\.openURL, OpenURLAction { url in
+                    guard let subslashlink = url.toSubSlashlinkURL() else {
+                        return .systemAction
+                    }
+
+                    return .handled
+                })
+            
+            Spacer()
+        }
+        .padding(AppTheme.padding)
+        .allowsHitTesting(false)
+        .frame(width: 374, height: 420)
+        .background(color)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.1), radius: 5)
 //            .scaleEffect(CGSize(width: 1.0, height: 1.0))
     }
 }
@@ -54,7 +82,11 @@ struct CardStack: View {
                         CardView(entry: card)
                         .offset(x: 0, y: sqrt(t) * 20) // CGFloat(index) * -10)
                         .offset(x: offset(for: card), y: 0)
-                        .rotationEffect(.degrees(Double(offset(for: card) / 32.0)))
+                        .rotationEffect(
+                            index != pointer
+                                ? .degrees(2.0*Double(sin(CGFloat(index) * 2.0)))
+                                : .degrees(Double(offset(for: card) / 32.0))
+                        )
                         .gesture(DragGesture()
                                     .onChanged { gesture in
                                         withAnimation(.interactiveSpring()) {
@@ -85,7 +117,7 @@ struct CardStack: View {
                         .zIndex(offset(for: card) == 0 ? 0 : 1) // Bring card to front during drag
                         .disabled(index != pointer)
                         .opacity(index >= pointer ? 1 : 0)
-                        .animation(.easeOutCubic(duration: 0.1), value: pointer)
+                        .animation(.easeOutCubic(duration: 0.2), value: pointer)
                     }
                 }
             }
