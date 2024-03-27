0f61097fa44560e7bb9f89f45c6011c301f1ef02
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-03-27 16:41:50 +1000

    Extract FullscreenSheetView

--- xcode/Subconscious/Shared/Components/Editor/EditorModalSheetView.swift
+++ xcode/Subconscious/Shared/Components/Editor/EditorModalSheetView.swift
@@ -104,11 +104,7 @@ struct EditorModalSheetView: View {
     )
     
     var namespace: Namespace.ID
-    @State var dragAmount: CGFloat = 0
-    private static let dragThreshold: CGFloat = 64
-    private static let discardThrowDistance: CGFloat = 1024
-    private static let discardThrowDelay: CGFloat = 0.15
-
+    
     func onDismiss() {
         store.send(.dismiss)
     }
@@ -120,144 +116,125 @@ struct EditorModalSheetView: View {
     
     var body: some View {
         if let item = store.state.item {
-            VStack(spacing: 0) {
-                HStack(spacing: 0) {
-                    Button(
-                        action: onDismiss,
-                        label: { Image(systemName: "multiply").bold() }
-                    )
-                    
-                    Spacer()
-                    
-                    if editor.state.audience == .local {
-                        Button(
-                            action: onPost,
-                            label: { Text(String(localized: "Post")).bold() }
-                        )
-                    } else if editor.state.saveState == .unsaved {
-                        Button(
-                            action: onDismiss,
-                            label: { Text(String(localized: "Save")).bold() }
-                        )
-                    }
-                }
-                .padding(AppTheme.padding)
-                .frame(height: AppTheme.minTouchSize)
-                .foregroundStyle(editor.state.highlight)
-                .tint(editor.state.highlight)
-                .background(editor.state.background)
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            dragAmount = gesture.translation.height
-                        }
-                        .onEnded { _ in
-                            if dragAmount > Self.dragThreshold {
-                                dragAmount = Self.discardThrowDistance
-                                DispatchQueue.main.asyncAfter(deadline: .now() + Self.discardThrowDelay) {
-                                    onDismiss()
-                                }
-                            } else {
-                                dragAmount = 0
-                            }
-                        }
-                )
-                
-                ZStack {
-                    // Heinous workaround for a bug with keyboard toolbars in ZStacks
-                    // https://stackoverflow.com/questions/71206502/keyboard-toolbar-buttons-not-showing
-                    NavigationStack {
-                        EditorModalSheetDetailView(
-                            app: app,
-                            store: editor,
-                            description: MemoEditorDetailDescription(
-                                address: item.address
-                            ),
-                            notify: { notification in
-                                guard let action = AppAction.from(notification) else {
-                                    return
-                                }
-                                
-                                app.send(action)
-                            }
-                        )
-                        .padding(0)
-                        .background(editor.state.background)
-                        .frame(maxHeight: .infinity)
-                        .disabled(!store.state.presented)
-                        .allowsHitTesting(store.state.presented)
-                    }
-                    
-                    VStack {
-                        Spacer()
-                            .allowsHitTesting(false)
+            FullscreenSheetView(
+                onDismiss: onDismiss,
+                toolbar: {
+                    HStack(spacing: 0) {
+                        CloseButtonView(action: onDismiss)
                         
-                        ZStack(alignment: .bottom) {
-                            // Gradient background for the bottom toolbar
-                            Rectangle()
-                                .foregroundStyle(editor.state.background)
-                                .frame(maxHeight: AppTheme.minGradientMaskSize)
-                                .mask(
-                                    LinearGradient(
-                                        gradient: Gradient(
-                                            colors: [.clear, .black, .black]
-                                        ),
-                                        startPoint: .top,
-                                        endPoint: .bottom
-                                    )
-                                )
-                            
-                            HStack(spacing: 0) {
-                                Button(
-                                    action: {
-                                        editor.send(.presentMetaSheet(true))
-                                    },
-                                    label: {
-                                        HStack(spacing: AppTheme.unit2) {
-                                            Image(audience: editor.state.audience)
-                                                .fontWeight(.medium)
-                                                .font(.caption)
-                                            
-                                            Text("\(editor.state.address?.markup ?? "-")")
-                                                .lineLimit(1)
-                                                .fontWeight(.medium)
-                                                .font(.caption)
-                                        }
-                                        .frame(alignment: .leading)
-                                    }
-                                )
-                                
-                                Spacer()
-                                
-                                Button(
-                                    action: {
-                                        editor.send(.presentMetaSheet(true))
-                                    },
-                                    label: {
-                                        Image(
-                                            systemName: "ellipsis"
-                                        )
-                                    }
-                                )
-                            }
-                            .foregroundColor(editor.state.highlight)
-                            .padding(
-                                EdgeInsets(
-                                    top: DeckTheme.cardPadding,
-                                    leading: DeckTheme.cardPadding,
-                                    bottom: 2 * DeckTheme.cardPadding,
-                                    trailing: DeckTheme.cardPadding
-                                )
+                        Spacer()
+                        
+                        if editor.state.audience == .local {
+                            Button(
+                                action: onPost,
+                                label: { Text(String(localized: "Post")).bold() }
+                            )
+                        } else if editor.state.saveState == .unsaved {
+                            Button(
+                                action: onDismiss,
+                                label: { Text(String(localized: "Save")).bold() }
                             )
                         }
                     }
+                    .padding(.horizontal, AppTheme.tightPadding)
+                    .frame(height: AppTheme.comfortableTouchSize)
+                    .foregroundStyle(editor.state.highlight)
+                    .tint(editor.state.highlight)
+                    .background(editor.state.background)
+                },
+                content: {
+                    ZStack {
+                        // Heinous workaround for a bug with keyboard toolbars in ZStacks
+                        // https://stackoverflow.com/questions/71206502/keyboard-toolbar-buttons-not-showing
+                        NavigationStack {
+                            EditorModalSheetDetailView(
+                                app: app,
+                                store: editor,
+                                description: MemoEditorDetailDescription(
+                                    address: item.address
+                                ),
+                                notify: { notification in
+                                    guard let action = AppAction.from(notification) else {
+                                        return
+                                    }
+                                    
+                                    app.send(action)
+                                }
+                            )
+                            .padding(0)
+                            .background(editor.state.background)
+                            .frame(maxHeight: .infinity)
+                            .disabled(!store.state.presented)
+                            .allowsHitTesting(store.state.presented)
+                        }
+                        
+                        VStack {
+                            Spacer()
+                                .allowsHitTesting(false)
+                            
+                            ZStack(alignment: .bottom) {
+                                // Gradient background for the bottom toolbar
+                                Rectangle()
+                                    .foregroundStyle(editor.state.background)
+                                    .frame(maxHeight: AppTheme.minGradientMaskSize)
+                                    .mask(
+                                        LinearGradient(
+                                            gradient: Gradient(
+                                                colors: [.clear, .black, .black]
+                                            ),
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                        )
+                                    )
+                                
+                                HStack(spacing: 0) {
+                                    Button(
+                                        action: {
+                                            editor.send(.presentMetaSheet(true))
+                                        },
+                                        label: {
+                                            HStack(spacing: AppTheme.unit2) {
+                                                Image(audience: editor.state.audience)
+                                                    .fontWeight(.medium)
+                                                    .font(.caption)
+                                                
+                                                Text("\(editor.state.address?.markup ?? "-")")
+                                                    .lineLimit(1)
+                                                    .fontWeight(.medium)
+                                                    .font(.caption)
+                                            }
+                                            .frame(alignment: .leading)
+                                        }
+                                    )
+                                    
+                                    Spacer()
+                                    
+                                    Button(
+                                        action: {
+                                            editor.send(.presentMetaSheet(true))
+                                        },
+                                        label: {
+                                            Image(
+                                                systemName: "ellipsis"
+                                            )
+                                        }
+                                    )
+                                }
+                                .foregroundColor(editor.state.highlight)
+                                .padding(
+                                    EdgeInsets(
+                                        top: DeckTheme.cardPadding,
+                                        leading: DeckTheme.cardPadding,
+                                        bottom: 2 * DeckTheme.cardPadding,
+                                        trailing: DeckTheme.cardPadding
+                                    )
+                                )
+                            }
+                        }
+                    }
                 }
-            }
-            .background(editor.state.background)
-            .cornerRadius(AppTheme.cornerRadiusLg, corners: [.topLeft, .topRight])
-            .offset(y: dragAmount)
+            )
             .matchedGeometryEffect(id: item.id, in: namespace, isSource: false)
-            .animation(.interactiveSpring(), value: dragAmount)
         }
     }
 }
